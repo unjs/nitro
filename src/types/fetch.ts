80c5d8913a0fb8a71571e888d81d33b2cbf028f9
Daniel Roe <daniel@roe.dev>
2022-03-29 23:19:48 +0100

    fix: export types rather than declaring them

--- src/types/fetch.ts
+++ src/types/fetch.ts
@@ -1,18 +1,18 @@
 import type { FetchRequest, FetchOptions, FetchResponse } from 'ohmyfetch'
 
 // An interface to extend in a local project
-export declare interface InternalApi { }
+export interface InternalApi { }
 
-export declare type ValueOf<C> = C extends Record<any, any> ? C[keyof C] : never
+export type ValueOf<C> = C extends Record<any, any> ? C[keyof C] : never
 
-export declare type MatchedRoutes<Route extends string> = ValueOf<{
+export type MatchedRoutes<Route extends string> = ValueOf<{
   // exact match, prefix match or root middleware
   [key in keyof InternalApi]: Route extends key | `${key}/${string}` | '/' ? key : never
 }>
 
-export declare type MiddlewareOf<Route extends string> = Exclude<InternalApi[MatchedRoutes<Route>], Error | void>
+export type MiddlewareOf<Route extends string> = Exclude<InternalApi[MatchedRoutes<Route>], Error | void>
 
-export declare type TypedInternalResponse<Route, Default> =
+export type TypedInternalResponse<Route, Default> =
   Default extends string | boolean | number | null | void | object
     // Allow user overrides
     ? Default
@@ -23,7 +23,7 @@ export declare type TypedInternalResponse<Route, Default> =
         : MiddlewareOf<Route>
       : Default
 
-export declare interface $Fetch<DefaultT = unknown, DefaultR extends FetchRequest = FetchRequest> {
+export interface $Fetch<DefaultT = unknown, DefaultR extends FetchRequest = FetchRequest> {
   <T = DefaultT, R extends FetchRequest = DefaultR> (request: R, opts?: FetchOptions): Promise<TypedInternalResponse<R, T>>
   raw<T = DefaultT, R extends FetchRequest = DefaultR> (request: R, opts?: FetchOptions): Promise<FetchResponse<TypedInternalResponse<R, T>>>
 }
