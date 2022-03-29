639f1abc29e219cf8143355b78ab24b92f930c19
Daniel Roe <daniel@roe.dev>
2022-03-29 22:11:42 +0100

    fix: reorder test

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -146,8 +146,8 @@ export function readPackageJson (
 export function resolveAliases (aliases: Record<string, string>) {
   for (const key in aliases) {
     for (const alias in aliases) {
-      if (alias === '@' && !aliases[key].startsWith('@/')) { continue } // Don't resolve @foo/bar
       if (!['~', '@', '#'].includes(alias[0])) { continue }
+      if (alias === '@' && !aliases[key].startsWith('@/')) { continue } // Don't resolve @foo/bar
 
       if (aliases[key].startsWith(alias)) {
         aliases[key] = aliases[alias] + aliases[key].slice(alias.length)
