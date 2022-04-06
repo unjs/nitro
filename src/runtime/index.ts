3dc2ee6d92bec9f4ece00f457887303053379298
Pooya Parsa <pyapar@gmail.com>
2022-04-06 11:20:53 +0200

    feat: reexport common utils from `#nitro` index

---
+++ src/runtime/index.ts
@@ -0,0 +1,5 @@
+export { storage } from '#nitro/virtual/storage'
+export { readAsset, statAsset } from '#nitro/virtual/assets'
+export { config } from './config'
+export * from './cache'
+export * from './paths'
