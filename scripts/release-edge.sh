b74a4f36499735201c4bfc05ff473eb538c0cc87
pooya parsa <pyapar@gmail.com>
2022-04-13 00:02:45 +0200

    refactor: switch to pnpm (#99)

--- scripts/release-edge.sh
+++ scripts/release-edge.sh
@@ -8,20 +8,20 @@ set -xe
 git restore -s@ -SW  -- .
 
 # Bump versions to edge
-yarn jiti ./scripts/bump-edge
+pnpm jiti ./scripts/bump-edge
 
-# Resolve yarn
-YARN_ENABLE_IMMUTABLE_INSTALLS=false yarn install
+# Resolve lockfile
+pnpm install
 
 # Update token
 if [[ ! -z ${NODE_AUTH_TOKEN} ]] ; then
   echo "//registry.npmjs.org/:_authToken=${NODE_AUTH_TOKEN}" >> ~/.npmrc
   echo "registry=https://registry.npmjs.org/" >> ~/.npmrc
   echo "always-auth=true" >> ~/.npmrc
-  echo "npmAuthToken: ${NODE_AUTH_TOKEN}" >> ~/.yarnrc.yml
+  echo "npmAuthToken: ${NODE_AUTH_TOKEN}" >> ~/.npmrc.yml
   npm whoami
 fi
 
 # Release packages
 echo "Publishing package..."
-yarn npm publish --access public --tolerate-republish
+npm publish --access public --tolerate-republish
