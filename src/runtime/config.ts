ad2766843b6161035479d1acb99fc374a0694d33
Daniel Roe <daniel@roe.dev>
2022-04-12 10:20:15 +0100

    refactor: use `createDefu` custom merger

--- src/runtime/config.ts
+++ src/runtime/config.ts
@@ -1,5 +1,6 @@
 import destr from 'destr'
 import { snakeCase } from 'scule'
+import { createDefu } from 'defu'
 
 // Bundled runtime config (injected by nitro)
 const _runtimeConfig = process.env.RUNTIME_CONFIG as any
@@ -13,23 +14,17 @@ const getEnv = (key: string) => {
   return destr(process.env[ENV_PREFIX + envKey] ?? process.env[ENV_PREFIX_ALT + envKey])
 }
 
-function overrideConfig (key: string, obj: unknown) {
-  if (obj && typeof obj === 'object') {
-    for (const subkey in obj) {
-      // key: { subKey } can be overridden by KEY_SUB_KEY`
-      obj[subkey] = getEnv(`${key}_${subkey}`) ?? obj[subkey]
-      overrideConfig(`${key}_${subkey}`, obj[subkey])
-    }
+const mergeWithEnvVariables = createDefu((obj: Record<string, any>, key: any, _value, namespace) => {
+  // key: { subKey } can be overridden by KEY_SUB_KEY`
+  const override = getEnv(namespace ? `${namespace}_${key}` : key)
+  if (override !== undefined) {
+    obj[key] = override
+    return true
   }
-}
-
-for (const key in _runtimeConfig) {
-  _runtimeConfig[key] = getEnv(key) ?? _runtimeConfig[key]
-  overrideConfig(key, _runtimeConfig[key])
-}
+})
 
 // Named exports
-const config = deepFreeze(_runtimeConfig)
+const config = deepFreeze(mergeWithEnvVariables(_runtimeConfig, _runtimeConfig))
 export const useRuntimeConfig = () => config
 export default config
 
