8a1728119d8a07e88b3aef801040cb5608e4f9fe
Tristan Lee <tristanlee85@gmail.com>
2022-04-12 12:30:54 -0400

    Write layer0 assets to output path

--- src/presets/layer0.ts
+++ src/presets/layer0.ts
@@ -3,31 +3,45 @@ import fs from 'fs-extra'
 import { defineNitroPreset } from '../preset'
 import { writeFile } from '../utils'
 
+// Layer0 entrypoint
 const entryTemplate = outputDir => `
 const http = require('http')
 
 module.exports = async function prod(port) {
-  const { handler } = await import('../${outputDir}/index.mjs')
+  const { handler } = await import('${outputDir}/index.mjs')
   const server = http.createServer(handler)
   server.listen(port)
 }
 `
 
+// Layer0 config
 const configFilename = 'layer0.config.js'
-const configTemplate = ({ connectorPath, appName, outputDir }) => `
+const configTemplate = ({ connectorPath, appName, includeDirs = [] }) => `
 // This file was automatically added by Layer0.
 // You should commit this file to source control.
 module.exports = {
   connector: "${connectorPath}",
 
+  // The name of the site in Layer0 to which this app should be deployed.
+  name: '${appName}',
+
+  // Overrides the default path to the routes file. The path should be relative to the root of your app.
+  routes: '${routerFilename}',
+
+  includeFiles: {
+    ${includeDirs.map(
+      (dir, idx) => (idx ? '\n\t\t' : '') + "'" + dir + "/**/*': true"
+    )}
+  },
+
   backends: {
     // origin: {
     //   // The domain name or IP address of the origin server
-    //   domainOrIp: "example.com",
+    //   domainOrIp: 'example.com',
 
     //   // When provided, the following value will be sent as the host header when connecting to the origin.
     //   // If omitted, the host header from the browser will be forwarded to the origin.
-    //   hostHeader: "example.com",
+    //   hostHeader: 'example.com',
 
     //   // Uncomment the following line if TLS is not set up properly on the origin domain and you want to ignore TLS errors
     //   // disableCheckCert: true,
@@ -38,14 +52,8 @@ module.exports = {
     // },
   },
 
-  // The name of the site in Layer0 to which this app should be deployed.
-  name: "${appName}",
-
   // The name of the team in Layer0 to which this app should be deployed.
   // team: 'my-team-name',
-
-  // Overrides the default path to the routes file. The path should be relative to the root of your app.
-  // routes: 'routes.js',
 
   // The maximum number of URLs that will be concurrently prendered during deployment when static prerendering is enabled.
   // Defaults to 200, which is the maximum allowed value.
@@ -65,13 +73,10 @@ module.exports = {
   // is uploaded during deployment
   //
   // includeNodeModules: true,
-
-  includeFiles: {
-    "${outputDir}/**/*": true,
-  }
 };
 `
 
+// Layer0 router
 const routerFilename = 'routes.js'
 const routerTemplate = `
 import { Router } from '@layer0/core'
@@ -92,46 +97,48 @@ export const layer0 = defineNitroPreset({
   },
   hooks: {
     async 'nitro:compiled' (nitro) {
+      // nitro output paths
+      const {
+        output: { dir: outputDir, serverDir, ...outputDirs },
+        rootDir
+      } = nitro.options
+
+      // relative output paths for Layer0 assets
       const connectorPathname = './layer0'
-      const appName =
-        (await fs.readJSON(join(nitro.options.rootDir, 'package.json'))).name ||
-        nitro.options.rootDir.split('/').reverse()[0]
-      const connectorPath = resolve(
-        nitro.options.rootDir,
-        connectorPathname,
-        'prod.js'
+      const layer0OutputDir = outputDir
+      const layer0EntryDir = resolve(layer0OutputDir, connectorPathname)
+      const connectorPath = resolve(layer0EntryDir, 'prod.js')
+      const configPath = resolve(layer0OutputDir, configFilename)
+      const routerPath = resolve(layer0OutputDir, routerFilename)
+
+      // package.json contents
+      const packageJson = await fs.readJSON(join(rootDir, 'package.json'))
+
+      // Layer0 app name
+      const appName = packageJson.name || rootDir.split('/').reverse()[0]
+
+      // write Layer0 config, router, and connector files
+      await writeFile(
+        configPath,
+        configTemplate({
+          connectorPath: connectorPathname,
+          appName,
+          includeDirs: Object.keys(outputDirs).map(dir =>
+            relative(layer0OutputDir, outputDirs[dir])
+          )
+        })
       )
-      const configPath = resolve(nitro.options.rootDir, configFilename)
-      const routerPath = resolve(nitro.options.rootDir, routerFilename)
-      const outputDir = relative(
-        nitro.options.rootDir,
-        nitro.options.output.dir
+      await writeFile(routerPath, routerTemplate)
+      await writeFile(
+        connectorPath,
+        entryTemplate(relative(layer0EntryDir, serverDir))
       )
-      const serverOutputDir = relative(
-        nitro.options.rootDir,
-        nitro.options.output.serverDir
+
+      // copy over app package.json necessary for `layer0 init`
+      await fs.copyFile(
+        join(rootDir, 'package.json'),
+        join(layer0OutputDir, 'package.json')
       )
-      const layer0ConfigExists = await fs.pathExists(configPath)
-      const layer0RouterExists = await fs.pathExists(routerPath)
-
-      await fs.ensureDir(resolve(nitro.options.rootDir, connectorPathname))
-
-      if (!layer0ConfigExists) {
-        await writeFile(
-          configPath,
-          configTemplate({
-            connectorPath: connectorPathname,
-            appName,
-            outputDir
-          })
-        )
-      }
-
-      if (!layer0RouterExists) {
-        await writeFile(routerPath, routerTemplate)
-      }
-
-      await writeFile(connectorPath, entryTemplate(serverOutputDir))
     }
   }
 })
