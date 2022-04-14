d7512befd26880232c767d5fc135b4db86091853
Pooya Parsa <pyapar@gmail.com>
2022-04-14 22:47:51 +0200

    feat(cache): rewrite `defineCachedEventHandler`

--- src/runtime/cache.ts
+++ src/runtime/cache.ts
@@ -1,5 +1,5 @@
 import { hash } from 'ohash'
-import { H3Response, toEventHandler, handleCacheHeaders } from 'h3'
+import { H3Response, toEventHandler, handleCacheHeaders, defineEventHandler, createEvent } from 'h3'
 import type { CompatibilityEventHandler, CompatibilityEvent } from 'h3'
 import { useStorage } from '#nitro'
 
@@ -39,7 +39,7 @@ export function defineCachedFunction <T=any> (fn: ((...args) => T | Promise<T>),
   const integrity = hash([opts.integrity, fn, opts])
 
   async function get (key: string, resolver: () => T | Promise<T>): Promise<CacheEntry<T>> {
-    const cacheKey = [opts.base, group, name, key].filter(Boolean).join(':')
+    const cacheKey = [opts.base, group, name, key].filter(Boolean).join(':').replace(/:\/$/, ':index')
     const entry: CacheEntry<T> = await useStorage().getItem(cacheKey) as any || {}
 
     const ttl = (opts.magAge ?? opts.magAge ?? 0) * 1000
@@ -97,58 +97,107 @@ export interface ResponseCacheEntry {
 export function defineCachedEventHandler (handler: CompatibilityEventHandler, opts: Omit<CachifyOptions, 'getKey'> = defaultCacheOptions) {
   const _opts: CachifyOptions<ResponseCacheEntry> = {
     ...opts,
-    getKey: req => req.originalUrl || req.url,
+    getKey: (event) => {
+      return event.req.originalUrl || event.req.url
+    },
     group: opts.group || 'nitro/handlers',
     integrity: [
       opts.integrity,
       handler
-    ],
-    transform (entry, event: CompatibilityEvent) {
-      if (event.res.headersSent) {
-        // Event already handled -_-
-        return
-      }
-      // Check for cache headers
-      if (handleCacheHeaders(event, {
-        modifiedTime: new Date(entry.mtime),
-        etag: `W/"${hash(entry.value)}"`,
-        maxAge: opts.magAge
-      })) {
-        return
-      }
-      for (const header in entry.value.headers) {
-        event.res.setHeader(header, entry.value.headers[header])
-      }
-      const cacheControl = []
-      if (opts.swr) {
-        if (opts.magAge) {
-          cacheControl.push(`s-maxage=${opts.magAge / 1000}`)
-        }
-        cacheControl.push('stale-while-revalidate')
-      } else if (opts.magAge) {
-        cacheControl.push(`max-age=${opts.magAge / 1000}`)
-      }
-      if (cacheControl.length) {
-        event.res.setHeader('Cache-Control', cacheControl.join(', '))
-      }
-      if (entry.value.code) {
-        event.res.statusCode = entry.value.code
-      }
-      return entry.value.body
-    }
+    ]
   }
 
   const _handler = toEventHandler(handler)
-  return cachedFunction<ResponseCacheEntry>(async (event: CompatibilityEvent) => {
+  const _cachedHandler = cachedFunction<ResponseCacheEntry>(async (incomingEvent: CompatibilityEvent) => {
+    // Create proxies to avoid sharing state with user request
+    const reqProxy = cloneWithProxy(incomingEvent.req, { headers: {} })
+    const resHeaders: Record<string, number | string | string[]> = {}
+    const resProxy = cloneWithProxy(incomingEvent.res, {
+      statusCode: 200,
+      getHeader (name) { return resHeaders[name] },
+      setHeader (name, value) { resHeaders[name] = value as any; return this },
+      getHeaderNames () { return Object.keys(resHeaders) },
+      hasHeader (name) { return name in resHeaders },
+      removeHeader (name) { delete resHeaders[name] },
+      getHeaders () { return resHeaders }
+    })
+
+    // Call handler
+    const event = createEvent(reqProxy, resProxy)
     const body = await _handler(event)
+
+    // Collect cachable headers
     const headers = event.res.getHeaders()
+    headers.Etag = `W/"${hash(body)}"`
+    headers['Last-Modified'] = new Date().toUTCString()
+    const cacheControl = []
+    if (opts.swr) {
+      if (opts.magAge) {
+        cacheControl.push(`s-maxage=${opts.magAge}`)
+      }
+      cacheControl.push('stale-while-revalidate')
+    } else if (opts.magAge) {
+      cacheControl.push(`max-age=${opts.magAge}`)
+    }
+    if (cacheControl.length) {
+      headers['Cache-Control'] = cacheControl.join(', ')
+    }
+
+    // Create cache entry for response
     const cacheEntry: ResponseCacheEntry = {
       code: event.res.statusCode,
       headers,
       body
     }
+
     return cacheEntry
   }, _opts)
+
+  return defineEventHandler(async (event) => {
+    // Call with cache
+    const response = await _cachedHandler(event)
+
+    // Don't continue if response is already handled by user
+    if (event.res.headersSent || event.res.writableEnded) {
+      return response.body
+    }
+
+    // Check for cache headers
+    if (handleCacheHeaders(event, {
+      modifiedTime: new Date(response.headers['Last-Modified'] as string),
+      etag: response.headers.etag as string,
+      maxAge: opts.magAge
+    })) {
+      return
+    }
+
+    // Send status and headers
+    event.res.statusCode = response.code
+    for (const name in response.headers) {
+      event.res.setHeader(name, response.headers[name])
+    }
+
+    // Send body
+    return response.body
+  })
+}
+
+function cloneWithProxy<T extends object = any> (obj: T, overrides: Partial<T>): T {
+  return new Proxy(obj, {
+    get (target, property, receiver) {
+      if (property in overrides) {
+        return overrides[property]
+      }
+      return Reflect.get(target, property, receiver)
+    },
+    set (target, property, value, receiver) {
+      if (property in overrides) {
+        overrides[property] = value
+        return true
+      }
+      return Reflect.set(target, property, value, receiver)
+    }
+  })
 }
 
 export const cachedEventHandler = defineCachedEventHandler
