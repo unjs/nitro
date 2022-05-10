e0928ce0f39e6af625be938e7f92164fd1c3ddfb
Pooya Parsa <pyapar@gmail.com>
2022-05-10 16:01:28 +0200

    enable only with `DEBUG` env

--- src/runtime/entries/node-server.ts
+++ src/runtime/entries/node-server.ts
@@ -23,7 +23,12 @@ server.listen(port, hostname, (err) => {
   console.log(`Listening on ${protocol}://${hostname}:${port}${useRuntimeConfig().app.baseURL}`)
 })
 
-process.on('unhandledRejection', err => console.error('[nitro] [dev] [unhandledRejection]', err))
-process.on('uncaughtException', err => console.error('[nitro] [dev] [uncaughtException]', err))
+if (process.env.DEBUG) {
+  process.on('unhandledRejection', err => console.error('[nitro] [dev] [unhandledRejection]', err))
+  process.on('uncaughtException', err => console.error('[nitro] [dev] [uncaughtException]', err))
+} else {
+  process.on('unhandledRejection', err => console.error('[nitro] [dev] [unhandledRejection] ' + err))
+  process.on('uncaughtException', err => console.error('[nitro] [dev] [uncaughtException] ' + err))
+}
 
 export default {}
