59f47c0fad66af38d8b59b3887661d0805740ae2
DaniFoldi <dani@stefan.hu>
2022-05-10 12:03:47 +0100

    fix: Reorganize structure

--- src/presets/cloudflare-pages.ts
+++ src/presets/cloudflare-pages.ts
@@ -1,3 +1,4 @@
+import { move } from 'fs-extra'
 import { resolve } from 'pathe'
 import { writeFile } from '../utils'
 import { defineNitroPreset } from '../preset'
@@ -7,17 +8,19 @@ export const cloudflarePages = defineNitroPreset({
   extends: 'cloudflare',
   entry: '#internal/nitro/entries/cloudflare-pages',
   commands: {
-    preview: 'npx wrangler pages dev'
+    preview: 'npx wrangler pages dev',
     // deployment is currently automatically done via GitHub/GitLab app
+    deploy: 'git push'
   },
   output: {
-    publicDir: '{{ rootDir }}',
     serverDir: '{{ rootDir }}/functions'
   },
   hooks: {
     async 'compiled' (nitro: Nitro) {
       await writeFile(resolve(nitro.options.output.dir, 'package.json'), JSON.stringify({ private: true, main: './functions/[[path]].js' }, null, 2))
       await writeFile(resolve(nitro.options.output.dir, 'package-lock.json'), JSON.stringify({ lockfileVersion: 1 }, null, 2))
+      await move(resolve(nitro.options.output.serverDir, 'index.mjs'), resolve(nitro.options.output.serverDir, '[[path]].mjs'))
+      await move(resolve(nitro.options.output.serverDir, 'index.mjs.map'), resolve(nitro.options.output.serverDir, '[[path]].mjs.map'))
     }
   }
 })
