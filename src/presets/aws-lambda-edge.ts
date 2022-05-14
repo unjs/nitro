bce177157b7e280e26e5548f5c55295332ba887a
WinterYukky <WinterYukky@gmail.com>
2022-05-14 16:40:37 +0000

    feat: add aws-lambda-edge preset

---
+++ src/presets/aws-lambda-edge.ts
@@ -0,0 +1,6 @@
+import { defineNitroPreset } from '../preset'
+
+export const awsLambdaEdge = defineNitroPreset({
+  entry: '#internal/nitro/entries/aws-lambda-edge',
+  externals: true
+})
---
+++ src/runtime/entries/aws-lambda-edge.ts
@@ -0,0 +1,31 @@
+import type { CloudFrontHeaders, Context, CloudFrontRequestEvent, CloudFrontResultResponse } from 'aws-lambda'
+import '#internal/nitro/virtual/polyfill'
+import { nitroApp } from '../app'
+
+export const handler = async function handler (event: CloudFrontRequestEvent, context: Context): Promise<CloudFrontResultResponse> {
+  const request = event.Records[0].cf.request
+
+  const r = await nitroApp.localCall({
+    event,
+    url: request.uri,
+    context,
+    headers: normalizeIncomingHeaders(request.headers),
+    method: request.method,
+    query: request.querystring,
+    body: request.body
+  })
+
+  return {
+    status: r.status.toString(),
+    headers: normalizeOutgoingHeaders(r.headers),
+    body: r.body.toString()
+  }
+}
+
+function normalizeIncomingHeaders (headers: CloudFrontHeaders) {
+  return Object.fromEntries(Object.entries(headers).map(([key, keyValues]) => [key, keyValues.map(kv => kv.value)]))
+}
+
+function normalizeOutgoingHeaders (headers: Record<string, string | string[] | undefined>): CloudFrontHeaders {
+  return Object.fromEntries(Object.entries(headers).map(([k, v]) => [k, Array.isArray(v) ? v.map(value => ({ value })) : [{ value: v }]]))
+}
