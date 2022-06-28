7022dd84ce41e25f40a8abd11c9ad4c6f0884340
Ahad Birang <farnabaz@gmail.com>
2022-06-28 13:30:15 +0200

    fix(azure): pass query params to local call

--- src/runtime/entries/azure.ts
+++ src/runtime/entries/azure.ts
@@ -6,7 +6,8 @@ export async function handle (context, req) {
   let url: string
   if (req.headers['x-ms-original-url']) {
     // This URL has been proxied as there was no static file matching it.
-    url = parseURL(req.headers['x-ms-original-url']).pathname
+    const parsedURL = parseURL(req.headers['x-ms-original-url'])
+    url = parsedURL.pathname + parsedURL.search
   } else {
     // Because Azure SWA handles /api/* calls differently they
     // never hit the proxy and we have to reconstitute the URL.
