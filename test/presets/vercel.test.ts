c584d15acb03a1559bb19b197e0942e91483f55b
Pooya Parsa <pooya@pi0.io>
2022-07-12 21:50:53 +0200

    wip

--- test/presets/vercel.test.ts
+++ test/presets/vercel.test.ts
@@ -14,3 +14,16 @@ describe('nitro:preset:vercel', async () => {
     }
   })
 })
+
+describe('nitro:preset:vercel', async () => {
+  const ctx = await setupTest('vercel-edge')
+  testNitro(ctx, async () => {
+    const handle = await import(resolve(ctx.outDir, 'functions/node/server/index.mjs'))
+      .then(r => r.default || r)
+    await startServer(ctx, handle)
+    return async ({ url }) => {
+      const res = await ctx.fetch(url)
+      return res
+    }
+  })
+})
