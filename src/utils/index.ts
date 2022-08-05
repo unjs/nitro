2a36b1e7390681092d10f0d2c308f7e651fa4e9a
Daniel Roe <daniel@roe.dev>
2022-08-05 16:45:46 +0100

    fix: sort aliases to ensure priority is given to more specific aliases (#388)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -132,7 +132,12 @@ export function readPackageJson (
   }
 }
 
-export function resolveAliases (aliases: Record<string, string>) {
+export function resolveAliases (_aliases: Record<string, string>) {
+  // Sort aliases from specific to general (ie. fs/promises before fs)
+  const aliases = Object.fromEntries(Object.entries(_aliases).sort(([a], [b]) =>
+    (b.split('/').length - a.split('/').length) || (b.length - a.length)
+  ))
+  // Resolve alias values in relation to each other
   for (const key in aliases) {
     for (const alias in aliases) {
       if (!['~', '@', '#'].includes(alias[0])) { continue }
