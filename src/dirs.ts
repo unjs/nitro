6cad6821039328e099b4b1a6cba5f73eba42a111
Pooya Parsa <pooya@pi0.io>
2022-08-11 12:32:03 +0200

    fix: fix dist dir check regex

--- src/dirs.ts
+++ src/dirs.ts
@@ -2,7 +2,7 @@ import { fileURLToPath } from 'url'
 import { dirname, resolve } from 'pathe'
 
 let distDir = dirname(fileURLToPath(import.meta.url))
-if (/\/[chunks|shared]$/.test(distDir)) {
+if (/(chunks|shared)$/.test(distDir)) {
   distDir = dirname(distDir)
 }
 export const pkgDir = resolve(distDir, '..')
