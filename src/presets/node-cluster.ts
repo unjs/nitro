565f2da93c21c025ad241515856cb5886f9a3619
Mastercuber <mr_w@posteo.de>
2022-09-01 02:32:01 +0200

    Write cluster starter to build dir, when compiled.

--- src/presets/node-cluster.ts
+++ src/presets/node-cluster.ts
@@ -1,10 +1,49 @@
 import { defineNitroPreset } from '../preset'
+import { dirname, resolve } from 'pathe'
+import { promises as fsp } from 'fs'
 
-export const nodeServer = defineNitroPreset({
+export const nodeCluster = defineNitroPreset({
   extends: 'node',
   entry: '#internal/nitro/entries/node-cluster',
   serveStatic: true,
+  output: {
+    dir: '{{ buildDir }}'
+  },
   commands: {
-    preview: 'node ./server/node-cluster.index.mjs'
+    preview: 'node ./node-cluster.index.mjs'
+  },
+  hooks: {
+    async 'compiled' (nitro) {
+      const path = resolve(nitro.options.output.dir, 'node-cluster.index.mjs')
+      await writeFile(path, entryTemplate())
+    }
   }
 })
+
+function entryTemplate () {
+  return `
+import os from 'node:os'
+import cluster from 'node:cluster'
+import { resolve } from 'pathe'
+
+export async function prod(port) {
+  const numCPUs = os.cpus().length
+  if (cluster.isPrimary) {
+    for (let i = 0; i < numCPUs; i++) {
+      cluster.fork()
+    }
+    cluster.on('exit', (worker, code, signal) => {
+      cluster.fork()
+    })
+  } else {
+    process.cwd()
+    import(resolve('..', 'server', 'index.mjs'))
+  }
+}
+  `.trim()
+}
+
+async function writeFile (path: string, contents: string) {
+  await fsp.mkdir(dirname(path), { recursive: true })
+  await fsp.writeFile(path, contents, 'utf-8')
+}
