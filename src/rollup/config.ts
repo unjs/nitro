1da2605091cbcaf3e242ad31846172d5d3bb1dcd
Mastercuber <mr_w@posteo.de>
2022-09-01 14:34:55 +0200

    Remove rollup Plugin

--- src/rollup/config.ts
+++ src/rollup/config.ts
@@ -32,7 +32,6 @@ import { esbuild } from './plugins/esbuild'
 import { raw } from './plugins/raw'
 import { storage } from './plugins/storage'
 import { importMeta } from './plugins/import-meta'
-import { cluster } from './plugins/cluster'
 
 export type RollupConfig = InputOptions & { output: OutputOptions }
 
@@ -194,11 +193,6 @@ export const getRollupConfig = (nitro: Nitro) => {
 
   // Handlers
   rollupConfig.plugins.push(handlers(nitro))
-
-  // Cluster
-  if (nitro.options.cluster?.enabled) {
-    rollupConfig.plugins.push(cluster(nitro))
-  }
 
   // Polyfill
   rollupConfig.plugins.push(virtual({
