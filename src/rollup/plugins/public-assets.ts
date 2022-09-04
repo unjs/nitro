0d503889a6e18a0b0466d52b39efbd80c01cb686
David Kubeš <kubesdavid@email.cz>
2022-09-04 14:35:21 +0200

    Improve compressPublicAssets

--- src/rollup/plugins/public-assets.ts
+++ src/rollup/plugins/public-assets.ts
@@ -28,7 +28,8 @@ export function publicAssets (nitro: Nitro): Plugin {
       })
       for (const id of files) {
         let type = mime.getType(id) || 'text/plain'
-        if (type.startsWith('text')) { type += '; charset=utf-8' }
+        const isTextType = type.startsWith('text')
+        if (isTextType) { type += '; charset=utf-8' }
         const fullPath = resolve(nitro.options.output.publicDir, id)
         const assetData = await fsp.readFile(fullPath)
         const etag = createEtag(assetData)
@@ -43,15 +44,27 @@ export function publicAssets (nitro: Nitro): Plugin {
           path: relative(nitro.options.output.serverDir, fullPath)
         }
 
-        if (nitro.options.compressPublicAssets && assetData.length > 1024 && !assetId.endsWith('.map')) {
-          for (const encoding of ['gzip', 'br']) {
+        if (!!nitro.options.compressPublicAssets && assetData.length > 1024 && !assetId.endsWith('.map') && isTypeCompressible(type)) {
+          let encodings = []
+          if (typeof nitro.options.compressPublicAssets === 'boolean') {
+            encodings = ['gzip', 'br']
+          } else {
+            const { gzip, brotli } = nitro.options.compressPublicAssets
+            if (gzip) { encodings.push('gzip') }
+            if (brotli) { encodings.push('br') }
+          }
+          for (const encoding of encodings) {
             const suffix = '.' + (encoding === 'gzip' ? 'gz' : 'br')
             const compressedPath = fullPath + suffix
-            const compressedBuff: Buffer = await new Promise((resolve, reject) => {
-              zlib[encoding === 'gzip' ? 'gzip' : 'brotliCompress'](assetData,
-                (error, result) => error ? reject(error) : resolve(result)
-              )
-            })
+            const compressedBuff = (encoding === 'gzip')
+              ? zlib.gzipSync(assetData, { level: zlib.constants.Z_BEST_COMPRESSION })
+              : zlib.brotliCompressSync(assetData, {
+                [zlib.constants.BROTLI_PARAM_MODE]: isTextType
+                  ? zlib.constants.BROTLI_MODE_TEXT
+                  : zlib.constants.BROTLI_MODE_GENERIC,
+                [zlib.constants.BROTLI_PARAM_QUALITY]: zlib.constants.BROTLI_MAX_QUALITY,
+                [zlib.constants.BROTLI_PARAM_SIZE_HINT]: assetData.length
+              })
             await fsp.writeFile(compressedPath, compressedBuff)
             assets[assetId + suffix] = {
               ...assets[assetId],
@@ -106,4 +119,33 @@ export function getAsset (id) {
 `
     }
   }, nitro.vfs)
+}
+
+const isTypeCompressible = (type: string): boolean => {
+  return [
+    'application/javascript',
+    'application/json',
+    'application/manifest+json',
+    'application/rss+xml',
+    'application/vnd.ms-fontobject',
+    'application/x-font-opentype',
+    'application/x-font-truetype',
+    'application/x-font-ttf',
+    'application/x-javascript',
+    'application/xml',
+    'application/xhtml+xml',
+    'font/eot',
+    'font/opentype',
+    'font/otf',
+    'font/truetype',
+    'image/svg+xml',
+    'image/vnd.microsoft.icon',
+    'image/x-icon',
+    'image/x-win-bitmap',
+    'text/css',
+    'text/javascript',
+    'text/plain',
+    'text/xml',
+    'text/x-component'
+  ].includes(type)
 }
