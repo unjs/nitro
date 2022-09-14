2f6ffeb7789d562d9aa4e317438974c5d27ffaf2
Daniel Roe <daniel@roe.dev>
2022-09-14 13:12:49 +0100

    test: update func name

--- test/presets/vercel.test.ts
+++ test/presets/vercel.test.ts
@@ -7,7 +7,7 @@ import { setupTest, startServer, testNitro } from '../tests'
 describe('nitro:preset:vercel', async () => {
   const ctx = await setupTest('vercel')
   testNitro(ctx, async () => {
-    const handle = await import(resolve(ctx.outDir, 'functions/index.func/index.mjs'))
+    const handle = await import(resolve(ctx.outDir, 'functions/__nuxt.func/index.mjs'))
       .then(r => r.default || r)
     await startServer(ctx, handle)
     return async ({ url }) => {
@@ -21,7 +21,7 @@ describe.skip('nitro:preset:vercel-edge', async () => {
   const ctx = await setupTest('vercel-edge')
   testNitro(ctx, async () => {
     // TODO: Add add-event-listener
-    const entry = resolve(ctx.outDir, 'functions/index.func/index.mjs')
+    const entry = resolve(ctx.outDir, 'functions/__nuxt.func/index.mjs')
     const entryCode = await fsp.readFile(entry, 'utf8')
     const runtime = new EdgeRuntime({ initialCode: entryCode })
     return async ({ url }) => {
