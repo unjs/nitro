e0c2b401132ee405e2c81bb3febca2badabf61e7
Pooya Parsa <pooya@pi0.io>
2022-09-19 15:52:34 +0200

    test: add ts-ignore

--- test/fixture/routes/modules.ts
+++ test/fixture/routes/modules.ts
@@ -1,5 +1,8 @@
+// @ts-ignore
 import depA from 'nitro-dep-a'
+// @ts-ignore
 import depB from 'nitro-dep-b'
+// @ts-ignore
 import depLib from 'nitro-lib'
 
 export default defineEventHandler(() => {
