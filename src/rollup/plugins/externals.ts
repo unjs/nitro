f427dd64a5c62ec807c1a508c9d6b15e416f923b
Pooya Parsa <pooya@pi0.io>
2022-09-19 15:48:05 +0200

    avoid duplicate writes

--- src/rollup/plugins/externals.ts
+++ src/rollup/plugins/externals.ts
@@ -1,5 +1,6 @@
 import { existsSync, promises as fsp } from 'fs'
 import { resolve, dirname, normalize, join, isAbsolute } from 'pathe'
+import consola from 'consola'
 import { nodeFileTrace, NodeFileTraceOptions } from '@vercel/nft'
 import type { Plugin } from 'rollup'
 import { resolvePath, isValidNodeImport, normalizeid } from 'mlly'
@@ -145,9 +146,12 @@ export function externals (opts: NodeExternalsOptions): Plugin {
       }
 
       // Trace files
-      const tracedFiles = await nodeFileTrace(Array.from(trackedExternals), opts.traceOptions)
+      let tracedFiles = await nodeFileTrace(Array.from(trackedExternals), opts.traceOptions)
         .then(r => Array.from(r.fileList).map(f => resolve(opts.traceOptions.base, f)))
         .then(r => r.filter(file => file.includes('node_modules')))
+
+      // Resolve symlinks
+      tracedFiles = await Promise.all(tracedFiles.map(file => fsp.realpath(file)))
 
       // Read package.json with cache
       const packageJSONCache = new Map() // pkgDir => contents
@@ -162,6 +166,8 @@ export function externals (opts: NodeExternalsOptions): Plugin {
 
       // Keep track of npm packages
       const tracedPackages = new Map() // name => pkgDir
+      const ignoreDirs = []
+      const ignoreWarns = new Set()
       for (const file of tracedFiles) {
         const { baseDir, pkgName } = parseNodeModulePath(file)
         if (!pkgName) {
@@ -174,20 +180,35 @@ export function externals (opts: NodeExternalsOptions): Plugin {
         if (existingPkgDir && existingPkgDir !== pkgDir) {
           const v1 = await getPackageJson(existingPkgDir).then(r => r.version)
           const v2 = await getPackageJson(pkgDir).then(r => r.version)
-          // if (semver.gte(v1, v2)) {
-          // Existing record is newer or same. Just skip.
-          // continue
-          // }
-          if (semver.major(v1) !== semver.major(v2)) {
-            console.warn(`Multiple major versions of package ${pkgName} are being externalized. Picking latest version.\n` + [
-              existingPkgDir + '@' + v1,
-              pkgDir + '@' + v2
-            ].map(p => '  - ' + p).join('\n'))
+          const isNewer = semver.gt(v2, v1)
+
+          // Warn about major version differences
+          const getMajor = v => v.split('.').filter(s => s !== '0')[0]
+          if (getMajor(v1) !== getMajor(v2)) {
+            const warn = `Multiple major versions of package \`${pkgName}\` are being externalized. Picking latest version:\n\n` + [
+              `  ${isNewer ? '-' : '+'} ` + existingPkgDir + '@' + v1,
+              `  ${isNewer ? '+' : '-'} ` + pkgDir + '@' + v2
+            ].join('\n')
+            if (!ignoreWarns.has(warn)) {
+              consola.warn(warn)
+              ignoreWarns.add(warn)
+            }
           }
+
+          // Exclude older version files
+          if (isNewer) {
+            ignoreDirs.push(existingPkgDir)
+          } else {
+            ignoreDirs.push(pkgDir)
+          }
+        } else {
+          // Add to traced packages
+          tracedPackages.set(pkgName, pkgDir)
         }
-        // Add to traced packages
-        tracedPackages.set(pkgName, pkgDir)
       }
+
+      // Filter out files from ignored packages
+      tracedFiles = tracedFiles.filter(f => !ignoreDirs.some(d => f.startsWith(d)))
 
       // Ensure all package.json files are traced
       for (const pkgDir of tracedPackages.values()) {
