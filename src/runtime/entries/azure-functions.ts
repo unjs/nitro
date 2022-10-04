a1c7bbb9fcd74ca291ec3e0e41f649df1480636e
mcremer-able <marc.cremer@able-group.de>
2022-10-04 10:50:52 +0200

    add a logging interceptor for azure context

--- src/runtime/entries/azure-functions.ts
+++ src/runtime/entries/azure-functions.ts
@@ -1,9 +1,28 @@
 import '#internal/nitro/virtual/polyfill'
 import { nitroApp } from '../app'
 
+const levels = ['log', 'info', 'warn', 'error']
+
+const higherOrderLog = (name: string, context) => {
+  const logFn = (params) => {
+    if (context[name]) {
+      context[name](...params)
+    } else if (context.log[name]) {
+      context.log[name](...params)
+    }
+  }
+
+  console[name] = logFn
+}
+/**
+ * intercepts all calls to console.* and redirects them to the azure context logger
+ */
+const interceptLogging = context =>
+  levels.forEach(m => higherOrderLog(m, context))
+
 export async function handle (context, req) {
   const url = '/' + (req.params.url || '')
-
+  interceptLogging(context)
   const { body, status, statusText, headers } = await nitroApp.localCall({
     url,
     headers: req.headers,
