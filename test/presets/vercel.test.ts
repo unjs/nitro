1d97b745d38410f0be3d02bf517a26e3ce85aeb0
Daniel Roe <daniel@roe.dev>
2022-10-11 12:29:09 +0100

    test: add assertions for vercel/netlify output

--- test/presets/vercel.test.ts
+++ test/presets/vercel.test.ts
@@ -1,6 +1,6 @@
 import { promises as fsp } from 'fs'
 import { resolve } from 'pathe'
-import { describe } from 'vitest'
+import { describe, it, expect } from 'vitest'
 import { EdgeRuntime } from 'edge-runtime'
 import { setupTest, startServer, testNitro } from '../tests'
 
@@ -14,6 +14,92 @@ describe('nitro:preset:vercel', async () => {
       const res = await ctx.fetch(url)
       return res
     }
+  })
+  it('should add route rules to config', async () => {
+    const config = await fsp.readFile(resolve(ctx.outDir, 'config.json'), 'utf-8')
+      .then(r => JSON.parse(r))
+    expect(config).toMatchInlineSnapshot(`
+      {
+        "overrides": {
+          "api/param/foo.json/index.html": {
+            "path": "api/param/foo.json",
+          },
+          "api/param/prerender1/index.html": {
+            "path": "api/param/prerender1",
+          },
+          "api/param/prerender2/index.html": {
+            "path": "api/param/prerender2",
+          },
+          "api/param/prerender3/index.html": {
+            "path": "api/param/prerender3",
+          },
+          "prerender/index.html": {
+            "path": "prerender",
+          },
+        },
+        "routes": [
+          {
+            "headers": {
+              "cache-control": "s-maxage=60",
+            },
+            "src": "/rules/headers",
+          },
+          {
+            "headers": {
+              "access-control-allow-headers": "*",
+              "access-control-allow-origin": "*",
+              "access-control-allowed-methods": "*",
+              "access-control-max-age": "0",
+            },
+            "src": "/rules/cors",
+          },
+          {
+            "headers": {
+              "Location": "/base",
+            },
+            "src": "/rules/redirect",
+            "status": 307,
+          },
+          {
+            "headers": {
+              "Location": "https://nitro.unjs.io/",
+            },
+            "src": "/rules/redirect/obj",
+            "status": 308,
+          },
+          {
+            "headers": {
+              "Location": "/base",
+              "x-test": "test",
+            },
+            "src": "/rules/nested/.*",
+            "status": 307,
+          },
+          {
+            "headers": {
+              "Location": "/other",
+            },
+            "src": "/rules/nested/override",
+            "status": 307,
+          },
+          {
+            "continue": true,
+            "headers": {
+              "cache-control": "public,max-age=31536000,immutable",
+            },
+            "src": "/build(.*)",
+          },
+          {
+            "handle": "filesystem",
+          },
+          {
+            "dest": "/__nitro",
+            "src": "/(.*)",
+          },
+        ],
+        "version": 3,
+      }
+    `)
   })
 })
 
