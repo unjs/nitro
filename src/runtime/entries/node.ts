9e0a6064c56dc38e6b0ff93400de75da94a20ad2
Pooya Parsa <pooya@pi0.io>
2022-10-15 15:21:44 +0200

    add handler as backward compat for node preset

--- src/runtime/entries/node.ts
+++ src/runtime/entries/node.ts
@@ -4,6 +4,9 @@ import { nitroApp } from '../app'
 
 export const listener = toNodeListener(nitroApp.h3App)
 
+/** @deprecated use new `listener` export instead */
+export const handler = listener
+
 if (process.env.DEBUG) {
   process.on('unhandledRejection', err => console.error('[nitro] [dev] [unhandledRejection]', err))
   process.on('uncaughtException', err => console.error('[nitro] [dev] [uncaughtException]', err))
