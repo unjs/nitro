bb831fa2b5105f86359089e784ebb615943696d9
Daniel Roe <daniel@roe.dev>
2022-10-17 00:39:24 +0100

    feat: add deno presets and entries

--- src/presets/deno.ts
+++ src/presets/deno.ts
@@ -1,3 +1,4 @@
+import MagicString from "magic-string";
 import { defineNitroPreset } from "../preset";
 
 export const deno = defineNitroPreset({
@@ -18,5 +19,46 @@ export const deno = defineNitroPreset({
       manualChunks: () => "index",
       format: "esm",
     },
+  },
+});
+
+export const denoServer = defineNitroPreset({
+  extends: "node-server",
+  entry: "#internal/nitro/entries/deno-server",
+  commands: {
+    preview:
+      "deno run --unstable --allow-net --allow-read --allow-env ./server/index.mjs",
+  },
+  rollupConfig: {
+    output: {
+      hoistTransitiveImports: false,
+    },
+    plugins: [
+      {
+        // TODO: deno plugin
+      },
+      {
+        name: "inject-process",
+        renderChunk(code, chunk) {
+          if (
+            !chunk.isEntry ||
+            code.includes("ROLLUP_NO_REPLACE") ||
+            !code.includes("process")
+          ) {
+            return;
+          }
+
+          const s = new MagicString(code);
+          s.prepend(
+            "import process from 'https://deno.land/std/node/process.ts'\n"
+          );
+
+          return {
+            code: s.toString(),
+            map: s.generateMap({ includeContent: true }),
+          };
+        },
+      },
+    ],
   },
 });
---
+++ src/runtime/entries/deno-server.ts
@@ -0,0 +1,42 @@
+import process from 'https://deno.land/std/node/process.ts'
+import { serve, serveTls } from 'https://deno.land/std/http/server.ts'
+
+import destr from 'destr'
+
+import { handler } from './deno'
+import { useRuntimeConfig } from '#internal/nitro'
+
+const cert = process.env.NITRO_SSL_CERT
+const key = process.env.NITRO_SSL_KEY
+const port = destr(process.env.NITRO_PORT || process.env.PORT) || 3e3
+const hostname = process.env.NITRO_HOST || process.env.HOST
+
+if (cert && key) {
+  serveTls(handler, { key, cert, port, hostname, onListen })
+} else {
+  serve(handler, { port, hostname, onListen })
+}
+
+function onListen ({ port, hostname }) {
+  const baseURL = (useRuntimeConfig().app.baseURL || '').replace(/\/$/, '')
+  const url = `${hostname}:${port}${baseURL}`
+  console.log(`Listening ${url}`)
+}
+
+if (process.env.DEBUG) {
+  process.on('unhandledRejection', err =>
+    console.error('[nitro] [dev] [unhandledRejection]', err)
+  )
+  process.on('uncaughtException', err =>
+    console.error('[nitro] [dev] [uncaughtException]', err)
+  )
+} else {
+  process.on('unhandledRejection', err =>
+    console.error('[nitro] [dev] [unhandledRejection] ' + err)
+  )
+  process.on('uncaughtException', err =>
+    console.error('[nitro] [dev] [uncaughtException] ' + err)
+  )
+}
+
+export default {}
