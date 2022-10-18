2a67edbe20145b83fbe0659afeacfc5d3828ac18
Daniel Roe <daniel@roe.dev>
2022-10-18 16:39:58 +0100

    fix: ignore deno globals

--- src/runtime/entries/deno-server.ts
+++ src/runtime/entries/deno-server.ts
@@ -3,9 +3,13 @@ import destr from 'destr'
 import { handler } from './deno'
 import { useRuntimeConfig } from '#internal/nitro'
 
+// @ts-ignore
 const cert = Deno.env.get('NITRO_SSL_CERT')
+// @ts-ignore
 const key = Deno.env.get('NITRO_SSL_KEY')
+// @ts-ignore
 const port = destr(Deno.env.get('NITRO_PORT') || Deno.env.get('PORT')) || 3e3
+// @ts-ignore
 const hostname = Deno.env.get('NITRO_HOST') || Deno.env.get('HOST')
 
 function onListen ({ port, hostname }) {
@@ -14,6 +18,7 @@ function onListen ({ port, hostname }) {
   console.log(`Listening ${url}`)
 }
 
+// @ts-ignore
 if (Deno.env.get('DEBUG')) {
   addEventListener('unhandledrejection', event =>
     console.error('[nitro] [dev] [unhandledRejection]', event.reason)
@@ -30,6 +35,7 @@ if (Deno.env.get('DEBUG')) {
   )
 }
 
+// @ts-ignore
 Deno.serve(handler, { key, cert, port, hostname, onListen })
 
 export default {}
