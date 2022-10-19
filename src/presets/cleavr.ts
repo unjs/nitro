6dff5b5caebd0854bf5f68c74f377efb53ab670b
Anish Ghimire <anishghimire862@gmail.com>
2022-10-19 22:45:11 +0545

    fix: remove cleavr entries

--- src/presets/cleavr.ts
+++ src/presets/cleavr.ts
@@ -1,6 +1,5 @@
 import { defineNitroPreset } from '../preset'
 
 export const cleavr = defineNitroPreset({
-  entry: '#internal/nitro/entries/cleavr',
   extends: 'node-server'
 })
--- src/runtime/entries/cleavr.ts
+++
@@ -1,5 +0,0 @@
-import '#internal/nitro/virtual/polyfill'
-import { toNodeListener } from 'h3'
-import { nitroApp } from '../app'
-
-export default toNodeListener(nitroApp.h3App)
