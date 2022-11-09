8691e16888c0d36a76ea8e4e57ff44bc7b92fd5e
Daniel Roe <daniel@roe.dev>
2022-11-09 10:46:26 +0100

    test: update ready promise

--- test/tests.ts
+++ test/tests.ts
@@ -78,9 +78,9 @@ export async function startDevServer (ctx: Context) {
   const devServer = createDevServer(ctx.nitro)
   ctx.server = await devServer.listen({})
   await prepare(ctx.nitro)
+  const ready = new Promise<void>((resolve) => { ctx.nitro.hooks.hook('dev:reload', () => resolve()) })
   await build(ctx.nitro)
-  // TODO: How to await that dev server is ready?!
-  await new Promise(resolve => setTimeout(resolve, 1000))
+  await ready
   console.log('>', ctx.server!.url)
 }
 
