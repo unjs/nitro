48b7fd63c9f2d856bd0c48ec6c538fa32ee198d9
WinterYukky <49480575+WinterYukky@users.noreply.github.com>
2022-11-13 22:04:39 +0900

    perf(aws-lambda): reduction cold start

--- src/presets/aws-lambda.ts
+++ src/presets/aws-lambda.ts
@@ -1,5 +1,6 @@
 import { defineNitroPreset } from '../preset'
 
 export const awsLambda = defineNitroPreset({
-  entry: '#internal/nitro/entries/aws-lambda'
+  entry: '#internal/nitro/entries/aws-lambda',
+  inlineDynamicImports: true
 })
