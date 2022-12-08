ed4436730326976ba30db16b79964a17f5d8d9aa
Pooya Parsa <pooya@pi0.io>
2022-12-08 17:11:47 +0100

    add leftover from merge

--- test/presets/nitro-dev.test.ts
+++ test/presets/nitro-dev.test.ts
@@ -1,25 +1,27 @@
-import { describe } from 'vitest'
-import { build, prepare } from '../../src/build.js'
-import { createDevServer } from '../../src/dev/server.js'
-import { setupTest, testNitro, Context } from '../tests'
+import { describe } from "vitest";
+import { build, prepare } from "../../src/build.js";
+import { createDevServer } from "../../src/dev/server.js";
+import { setupTest, testNitro, Context } from "../tests";
 
-describe('nitro:preset:nitro-dev', async () => {
-  const ctx = await setupTest('nitro-dev')
+describe("nitro:preset:nitro-dev", async () => {
+  const ctx = await setupTest("nitro-dev");
   testNitro(ctx, async () => {
-    await startDevServer(ctx)
+    await startDevServer(ctx);
     return async ({ url }) => {
-      const res = await ctx.fetch(url)
-      return res
-    }
-  })
-})
+      const res = await ctx.fetch(url);
+      return res;
+    };
+  });
+});
 
-async function startDevServer (ctx: Context) {
-  const devServer = createDevServer(ctx.nitro)
-  ctx.server = await devServer.listen({})
-  await prepare(ctx.nitro)
-  const ready = new Promise<void>((resolve) => { ctx.nitro.hooks.hook('dev:reload', () => resolve()) })
-  await build(ctx.nitro)
-  await ready
-  console.log('>', ctx.server!.url)
+async function startDevServer(ctx: Context) {
+  const devServer = createDevServer(ctx.nitro);
+  ctx.server = await devServer.listen({});
+  await prepare(ctx.nitro);
+  const ready = new Promise<void>((resolve) => {
+    ctx.nitro.hooks.hook("dev:reload", () => resolve());
+  });
+  await build(ctx.nitro);
+  await ready;
+  console.log(">", ctx.server!.url);
 }
