3cb1219fd5af2996f0651fdad0cd2ef0c7026255
Daniel Roe <daniel@roe.dev>
2022-12-17 20:53:45 +0000

    style: add line break

--- src/compress.ts
+++ src/compress.ts
@@ -71,7 +71,8 @@ function isTextMime(mimeType: string) {
   return /text|javascript|json|xml/.test(mimeType);
 }
 
-const COMPRESSIBLE_MIMES_RE = /atom|css|eot|htc|html|ico|js|json|mjs|otf|rss|svg|text|ttf|webmanifest|xml/;
+const COMPRESSIBLE_MIMES_RE =
+  /atom|css|eot|htc|html|ico|js|json|mjs|otf|rss|svg|text|ttf|webmanifest|xml/;
 
 function isCompressableMime(mimeType: string) {
   return COMPRESSIBLE_MIMES_RE.test(mimeType);
