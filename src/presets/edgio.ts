434aeb174753ac7f1732a224d684680d82449ef0
Rishi Raj Jain <rjain@llnw.com>
2023-01-10 16:35:28 +0530

    Remove @edgio/nitropack

--- src/presets/edgio.ts
+++ src/presets/edgio.ts
@@ -7,23 +7,43 @@ export const edgio = defineNitroPreset({
   extends: "node",
   commands: {
     deploy: "cd ./ && npm run edgio:deploy",
-    preview: "cd ./ && npm run edgio:preview",
   },
   hooks: {
     async compiled(nitro) {
       // Write Edgio config, router, and connector files
       const edgioConfig = {
-        connector: "@edgio/nitropack",
+        connector: "./edgio",
+        includeFiles: {
+          [`${nitro.options.output.serverDir}/**/*`]: true,
+        },
       };
+
+      // .output/edgio.config.js
       const configPath = resolve(nitro.options.output.dir, "edgio.config.js");
       await writeFile(
         configPath,
         `module.exports = ${JSON.stringify(edgioConfig, null, 2)}`
       );
 
+      // .output/routes.js
       const routerPath = resolve(nitro.options.output.dir, "routes.js");
-      await writeFile(routerPath, routesTemplate());
+      await writeFile(
+        routerPath,
+        routesTemplate(nitro.options.output.publicDir)
+      );
 
+      // .output/edgio/prod.js
+      const edgioServerlessPath = resolve(
+        nitro.options.output.dir,
+        "edgio",
+        "prod.js"
+      );
+      await writeFile(
+        edgioServerlessPath,
+        serverlessTemplate(nitro.options.output.serverDir)
+      );
+
+      // .output/package.json
       const pkgJSON: PackageJson & { scripts: Record<string, string> } = {
         private: true,
         scripts: {
@@ -33,9 +53,8 @@ export const edgio = defineNitroPreset({
           "edgio:preview": "edgio build && edgio run --production",
         },
         devDependencies: {
-          "@edgio/cli": "latest",
-          "@edgio/core": "latest",
-          "@edgio/nitropack": "latest",
+          "@edgio/cli": "^6",
+          "@edgio/core": "^6",
         },
       };
       await writeFile(
@@ -51,12 +70,37 @@ async function writeFile(path: string, contents: string) {
   await fsp.writeFile(path, contents, "utf8");
 }
 
-// Edgio router (.output/routes.js)
-function routesTemplate() {
+function routesTemplate(publicDir) {
   return `
 import { Router } from '@edgio/core/router'
-import { nitropackRoutes } from '@edgio/nitropack'
+import { isProductionBuild } from '@edgio/core/environment'
 
-export default new Router().use(nitropackRoutes)
+const router = new Router()
+
+if (isProductionBuild()) {
+  router.static('${publicDir}')
+}
+
+router.fallback(({ renderWithApp }) => { renderWithApp() })
+
+export defaulr router
+`.trim();
+}
+
+function serverlessTemplate(serverDir) {
+  return `
+import { join } from 'path'
+import { existsSync } from 'fs'
+
+module.exports = async (port) => {
+  const appFilePath = join(process.cwd(), ${serverDir})
+  // If ${serverDir}/index.mjs exist, run it
+  if (existsSync(appFilePath)) {
+    // Set the NITRO_PORT per
+    // https://github.com/nuxt/framework/discussions/4972#:~:text=the%20PORT%20or-,NITRO_PORT,-environment%20variables
+    process.env.NITRO_PORT = port.toString()
+    await import(appFilePath)
+  }
+}
 `.trim();
 }
