ae567c2bd7663841a209f236371427235ee4d3cb
Xin Du (Clark) <clark.duxin@gmail.com>
2023-01-12 14:02:46 +0000

    avoid diff

--- src/runtime/timing.ts
+++ src/runtime/timing.ts
@@ -8,30 +8,31 @@ export const globalTiming = globalThis.__timing__ || {
   metrics: [],
 };
 
-export const timingPlugin = defineNitroPlugin((nitro) => {
-  nitro.h3App.use(
-    eventHandler((event) => {
-      const start = globalTiming.start();
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Server-Timing
+export const timingMiddleware = eventHandler((event) => {
+  const start = globalTiming.start();
 
-      const _end = event.node.res.end;
-      event.node.res.end = function (
-        chunk: any,
-        encoding: BufferEncoding,
-        cb?: () => void
-      ) {
-        const metrics = [
-          ["Generate", globalTiming.end(start)],
-          ...globalTiming.metrics,
-        ];
-        const serverTiming = metrics
-          .map((m) => `-;dur=${m[1]};desc="${encodeURIComponent(m[0])}"`)
-          .join(", ");
-        if (!event.node.res.headersSent) {
-          event.node.res.setHeader("Server-Timing", serverTiming);
-        }
-        _end.call(event.node.res, chunk, encoding, cb);
-        return this;
-      }.bind(event.node.res);
-    })
-  );
+  const _end = event.node.res.end;
+  event.node.res.end = function (
+    chunk: any,
+    encoding: BufferEncoding,
+    cb?: () => void
+  ) {
+    const metrics = [
+      ["Generate", globalTiming.end(start)],
+      ...globalTiming.metrics,
+    ];
+    const serverTiming = metrics
+      .map((m) => `-;dur=${m[1]};desc="${encodeURIComponent(m[0])}"`)
+      .join(", ");
+    if (!event.node.res.headersSent) {
+      event.node.res.setHeader("Server-Timing", serverTiming);
+    }
+    _end.call(event.node.res, chunk, encoding, cb);
+    return this;
+  }.bind(event.node.res);
+});
+
+export const timingPlugin = defineNitroPlugin((nitro) => {
+  nitro.h3App.use(timingMiddleware);
 });
