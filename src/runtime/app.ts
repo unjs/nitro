5f366767f33228e66da32bbef3dbeea72bc0d4b6
Xin Du (Clark) <clark.duxin@gmail.com>
2023-01-12 13:56:42 +0000

    move timing to plugin

--- src/runtime/app.ts
+++ src/runtime/app.ts
@@ -14,7 +14,6 @@ import {
 } from "unenv/runtime/fetch/index";
 import { createHooks, Hookable } from "hookable";
 import { useRuntimeConfig } from "./config";
-import { timingMiddleware } from "./timing";
 import { cachedEventHandler } from "./cache";
 import { createRouteRulesHandler, getRouteRulesForPath } from "./route-rules";
 import { plugins } from "#internal/nitro/virtual/plugins";
@@ -39,10 +38,6 @@ function createNitroApp(): NitroApp {
     debug: destr(process.env.DEBUG),
     onError: errorHandler,
   });
-
-  if (config.timing) {
-    h3App.use(config.app.baseURL, timingMiddleware);
-  }
 
   const router = createRouter();
 
--- src/runtime/timing.ts
+++ src/runtime/timing.ts
@@ -1,32 +1,37 @@
 import { eventHandler } from "h3";
 
+import { defineNitroPlugin } from "./plugin";
+
 export const globalTiming = globalThis.__timing__ || {
   start: () => 0,
   end: () => 0,
   metrics: [],
 };
 
-// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Server-Timing
-export const timingMiddleware = eventHandler((event) => {
-  const start = globalTiming.start();
+export const timingPlugin = defineNitroPlugin((nitro) => {
+  nitro.h3App.use(
+    eventHandler((event) => {
+      const start = globalTiming.start();
 
-  const _end = event.res.end;
-  event.res.end = function (
-    chunk: any,
-    encoding: BufferEncoding,
-    cb?: () => void
-  ) {
-    const metrics = [
-      ["Generate", globalTiming.end(start)],
-      ...globalTiming.metrics,
-    ];
-    const serverTiming = metrics
-      .map((m) => `-;dur=${m[1]};desc="${encodeURIComponent(m[0])}"`)
-      .join(", ");
-    if (!event.res.headersSent) {
-      event.res.setHeader("Server-Timing", serverTiming);
-    }
-    _end.call(event.res, chunk, encoding, cb);
-    return this;
-  }.bind(event.res);
+      const _end = event.res.end;
+      event.res.end = function (
+        chunk: any,
+        encoding: BufferEncoding,
+        cb?: () => void
+      ) {
+        const metrics = [
+          ["Generate", globalTiming.end(start)],
+          ...globalTiming.metrics,
+        ];
+        const serverTiming = metrics
+          .map((m) => `-;dur=${m[1]};desc="${encodeURIComponent(m[0])}"`)
+          .join(", ");
+        if (!event.res.headersSent) {
+          event.res.setHeader("Server-Timing", serverTiming);
+        }
+        _end.call(event.res, chunk, encoding, cb);
+        return this;
+      }.bind(event.res);
+    })
+  );
 });
