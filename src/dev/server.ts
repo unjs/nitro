03a52ca24d0e47d1ae91bb705fc2b19dcaf30d6c
Mike Bellika <mike@altinget.dk>
2023-01-31 09:06:51 +0100

    fix: don't overwrite proxy headers if already set

--- src/dev/server.ts
+++ src/dev/server.ts
@@ -158,14 +158,16 @@ export function createDevServer(nitro: Nitro): NitroDevServer {
   // Main worker proxy
   const proxy = createProxy();
   proxy.proxy.on("proxyReq", (proxyReq, req) => {
-    // TODO: Avoid overwriting headers if already set
-    if (req.socket.remoteAddress) {
+    if (req.socket.remoteAddress && !proxyReq.getHeaders()["x-forwarded-for"]) {
       proxyReq.setHeader("X-Forwarded-For", req.socket.remoteAddress);
     }
-    if (req.socket.remotePort) {
+    if (req.socket.remotePort && !proxyReq.getHeaders()["x-forwarded-port"]) {
       proxyReq.setHeader("X-Forwarded-Port", req.socket.remotePort);
     }
-    if (req.socket.remoteFamily) {
+    if (
+      req.socket.remoteFamily &&
+      !proxyReq.getHeaders()["x-forwarded-proto"]
+    ) {
       proxyReq.setHeader("X-Forwarded-Proto", req.socket.remoteFamily);
     }
   });
