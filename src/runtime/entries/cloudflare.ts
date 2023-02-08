247f312723e2baffdfce3b0acf4ab0fdc08aea6b
Oleg Khalin <oleghalin@users.noreply.github.com>
2023-02-08 10:55:53 +0100

    feat(cloudflare): `cacheControl` support for public assets with with `maxAge` (#922)
    * fix(nitro): Cloudflare Workers KV Cache Control ttl for public assets
    
    * style: resolve prettier issues
    
    * feat: keep meta for public assets
    
    ---------
    
    Co-authored-by: Pooya Parsa <pooya@pi0.io>

--- src/runtime/entries/cloudflare.ts
+++ src/runtime/entries/cloudflare.ts
@@ -5,8 +5,9 @@ import {
 } from "@cloudflare/kv-asset-handler";
 import { withoutBase } from "ufo";
 import { requestHasBody } from "../utils";
-import { nitroApp } from "../app";
+import { nitroApp } from "#internal/nitro/app";
 import { useRuntimeConfig } from "#internal/nitro";
+import { getPublicAssetMeta } from "#internal/nitro/virtual/public-assets";
 
 addEventListener("fetch", (event: any) => {
   event.respondWith(handleEvent(event));
@@ -48,13 +49,14 @@ async function handleEvent(event: FetchEvent) {
 }
 
 function assetsCacheControl(_request) {
-  // TODO: Detect public asset bases
-  // if (request.url.startsWith(buildAssetsURL())) {
-  //   return {
-  //     browserTTL: 31536000,
-  //     edgeTTL: 31536000
-  //   }
-  // }
+  const url = new URL(_request.url);
+  const meta = getPublicAssetMeta(url.pathname);
+  if (meta.maxAge) {
+    return {
+      browserTTL: meta.maxAge,
+      edgeTTL: meta.maxAge,
+    };
+  }
   return {};
 }
 
