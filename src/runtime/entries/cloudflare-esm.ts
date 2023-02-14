007194a3ea370454e6eddd1906c4d6e662e464d5
Tim Hanlon <tim@timhanlon.com>
2023-02-14 17:40:59 +1100

    chore: update to match service worker preset

--- src/runtime/entries/cloudflare-esm.ts
+++ src/runtime/entries/cloudflare-esm.ts
@@ -1,107 +1,93 @@
-import '#internal/nitro/virtual/polyfill'
-import { getAssetFromKV, mapRequestToAsset } from '@cloudflare/kv-asset-handler'
-import { withoutBase } from 'ufo'
-import type { ExecutionContext, RequestInit } from '@cloudflare/workers-types'
-import { createCall } from 'unenv/runtime/fetch/index'
-import { NodeListener, App, createEvent, createError, isError, sendError, H3EventContext } from 'h3'
+import "#internal/nitro/virtual/polyfill";
+import {
+  getAssetFromKV,
+  mapRequestToAsset,
+} from "@cloudflare/kv-asset-handler";
+import { withoutBase } from "ufo";
+import type { ExecutionContext } from "@cloudflare/workers-types";
 // @ts-ignore Bundled by Wrangler 2. See https://github.com/cloudflare/kv-asset-handler#asset_manifest-required-for-es-modules
-import manifest from '__STATIC_CONTENT_MANIFEST'
-import { requestHasBody, useRequestBody } from '../utils'
-import { nitroApp } from '../app'
-import { useRuntimeConfig } from '#internal/nitro'
+import manifest from "__STATIC_CONTENT_MANIFEST";
+import { requestHasBody } from "../utils";
+import { nitroApp } from "#internal/nitro/app";
+import { useRuntimeConfig } from "#internal/nitro";
+import { getPublicAssetMeta } from "#internal/nitro/virtual/public-assets";
 
 export default {
-  async fetch (request, env, ctx: ExecutionContext) {
+  async fetch(request, env, ctx: ExecutionContext) {
     try {
       // TODO: Update to new API: https://github.com/cloudflare/kv-asset-handler#es-modules
-      return await getAssetFromKV({
-        request,
-        waitUntil (promise) {
-          return ctx.waitUntil(promise)
+      return await getAssetFromKV(
+        {
+          request,
+          waitUntil(promise) {
+            return ctx.waitUntil(promise);
+          },
+        },
+        {
+          cacheControl: assetsCacheControl,
+          mapRequestToAsset: baseURLModifier,
+          ASSET_NAMESPACE: env.__STATIC_CONTENT,
+          ASSET_MANIFEST: JSON.parse(manifest),
         }
-      }, {
-        cacheControl: assetsCacheControl,
-        mapRequestToAsset: baseURLModifier,
-        ASSET_NAMESPACE: env.__STATIC_CONTENT,
-        ASSET_MANIFEST: JSON.parse(manifest)
-      })
-    } catch (_err) {
+      );
+    } catch {
       // Ignore
     }
 
-    const url = new URL(request.url)
-    let body
+    const url = new URL(request.url);
+    let body;
     if (requestHasBody(request)) {
-      body = await useRequestBody(request)
+      body = Buffer.from(await request.arrayBuffer());
     }
 
-    const localCall = createCall(toNodeListener(nitroApp.h3App, {
-      cf: (request as unknown as RequestInit)?.cf,
-      env,
-      ctx
-    }) as any)
-
-    const r = await localCall({
+    const r = await nitroApp.localCall({
       event: { request },
+      context: {
+        // https://developers.cloudflare.com/workers//runtime-apis/request#incomingrequestcfproperties
+        cf: (request as any).cf,
+        env,
+        ctx,
+      },
       url: url.pathname + url.search,
       host: url.hostname,
       protocol: url.protocol,
       headers: Object.fromEntries(request.headers.entries()),
       method: request.method,
       redirect: request.redirect,
-      body
-    })
+      body,
+    });
 
     return new Response(r.body, {
       // @ts-ignore TODO: Should be HeadersInit instead of string[][]
       headers: normalizeOutgoingHeaders(r.headers),
       status: r.status,
-      statusText: r.statusText
-    })
-  }
-}
+      statusText: r.statusText,
+    });
+  },
+};
 
-function assetsCacheControl (_request) {
-  // TODO: Detect public asset bases
-  // if (request.url.startsWith(buildAssetsURL())) {
-  //   return {
-  //     browserTTL: 31536000,
-  //     edgeTTL: 31536000
-  //   }
-  // }
-  return {}
+function assetsCacheControl(_request) {
+  const url = new URL(_request.url);
+  const meta = getPublicAssetMeta(url.pathname);
+  if (meta.maxAge) {
+    return {
+      browserTTL: meta.maxAge,
+      edgeTTL: meta.maxAge,
+    };
+  }
+  return {};
 }
 
 const baseURLModifier = (request: Request) => {
-  const url = withoutBase(request.url, useRuntimeConfig().app.baseURL)
-  return mapRequestToAsset(new Request(url, request))
-}
+  const url = withoutBase(request.url, useRuntimeConfig().app.baseURL);
+  return mapRequestToAsset(new Request(url, request));
+};
 
-function normalizeOutgoingHeaders (headers: Record<string, string | string[] | undefined>) {
-  return Object.entries(headers).map(([k, v]) => [k, Array.isArray(v) ? v.join(',') : v])
-}
-
-function toNodeListener (app: App, context: H3EventContext): NodeListener {
-  const toNodeHandle: NodeListener = async function (req, res) {
-    const event = createEvent(req, res)
-    event.context = { ...event.context, ...context }
-    try {
-      await app.handler(event)
-    } catch (_error: any) {
-      const error = createError(_error)
-      if (!isError(_error)) {
-        error.unhandled = true
-      }
-
-      if (app.options.onError) {
-        await app.options.onError(error, event)
-      } else {
-        if (error.unhandled || error.fatal) {
-          console.error('[h3]', error.fatal ? '[fatal]' : '[unhandled]', error) // eslint-disable-line no-console
-        }
-        await sendError(event, error, !!app.options.debug)
-      }
-    }
-  }
-  return toNodeHandle
+function normalizeOutgoingHeaders(
+  headers: Record<string, string | string[] | undefined>
+) {
+  return Object.entries(headers).map(([k, v]) => [
+    k,
+    Array.isArray(v) ? v.join(",") : v,
+  ]);
 }
