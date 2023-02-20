303ce0547a230dca35e15a1f888762994819689a
Pooya Parsa <pooya@pi0.io>
2023-02-20 23:34:30 +0100

    shorten commands

--- src/presets/lagon.ts
+++ src/presets/lagon.ts
@@ -1,13 +1,14 @@
+import type { PackageJson } from "pkg-types";
+import { resolve } from "pathe";
 import { defineNitroPreset } from "../preset";
+import { writeFile } from "../utils";
 
 export const lagon = defineNitroPreset({
   extends: "base-worker",
   entry: "#internal/nitro/entries/lagon",
   commands: {
-    preview:
-      "npx -p esbuild -p @lagon/cli lagon dev ./server/index.mjs -p ./public",
-    deploy:
-      "npx -p esbuild -p @lagon/cli lagon deploy ./server/index.mjs -p ./public",
+    preview: "npm run dev --prefix ./",
+    deploy: "npm run deploy --prefix ./",
   },
   rollupConfig: {
     output: {
@@ -15,8 +16,27 @@ export const lagon = defineNitroPreset({
       format: "esm",
     },
   },
-  // TODO: write lagon config when it's supported
-  // hooks: {
-  //   async compiled(nitro) {},
-  // },
+
+  hooks: {
+    async compiled(nitro) {
+      // TODO: write lagon config when it's supported
+
+      // Write package.json for deployment
+      await writeFile(
+        resolve(nitro.options.output.dir, "package.json"),
+        JSON.stringify(
+          <PackageJson>{
+            private: true,
+            scripts: {
+              dev: "npx -p esbuild -p @lagon/cli lagon dev ./server/index.mjs -p ./public",
+              deploy:
+                "npx -p esbuild -p @lagon/cli lagon deploy ./server/index.mjs -p ./public",
+            },
+          },
+          null,
+          2
+        )
+      );
+    },
+  },
 });
