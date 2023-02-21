c82287a9afc7794ff1b7838cc487eb788d2734f4
Daniel Roe <daniel@roe.dev>
2023-02-21 12:32:49 +0000

    fix(cloudflare-pages): exclude assets from function call (#965)
    Co-authored-by: Pooya Parsa <pooya@pi0.io>

--- test/presets/cloudflare.test.ts
+++ test/presets/cloudflare.test.ts
@@ -1,6 +1,7 @@
+import { promises as fsp } from "node:fs";
 import { resolve } from "pathe";
 import { Miniflare } from "miniflare";
-import { describe } from "vitest";
+import { describe, it, expect } from "vitest";
 
 import { setupTest, testNitro } from "../tests";
 
@@ -18,5 +19,37 @@ describe("nitro:preset:cloudflare", async () => {
       });
       return res as unknown as Response;
     };
+  });
+});
+
+describe("nitro:preset:cloudflare-pages", async () => {
+  const ctx = await setupTest("cloudflare-pages");
+
+  it("should generate a _routes.json", async () => {
+    const config = await fsp
+      .readFile(resolve(ctx.outDir, "public/_routes.json"), "utf8")
+      .then((r) => JSON.parse(r));
+    expect(config).toMatchInlineSnapshot(`
+      {
+        "exclude": [
+          "/build/*",
+          "/favicon.ico",
+          "/icon.png",
+          "/api/hello",
+          "/prerender/index.html",
+          "/prerender/index.html.br",
+          "/prerender/index.html.gz",
+          "/api/hey/index.html",
+          "/api/param/foo.json/index.html",
+          "/api/param/prerender1/index.html",
+          "/api/param/prerender3/index.html",
+          "/api/param/prerender4/index.html",
+        ],
+        "include": [
+          "/*",
+        ],
+        "version": 1,
+      }
+    `);
   });
 });
