62d028d40620c662698842f755db69c37bdd05cf
Daniel Roe <daniel@roe.dev>
2023-03-13 22:29:28 +0000

    fix: don't simplify return type

--- src/types/fetch.ts
+++ src/types/fetch.ts
@@ -1,6 +1,6 @@
 import type { RouterMethod } from "h3";
 import type { FetchRequest, FetchOptions, FetchResponse } from "ofetch";
-import type { Serialize, Simplify } from "./serialize";
+import type { Serialize } from "./serialize";
 import type { MatchedRoutes } from "./utils";
 
 // An interface to extend in a local project
@@ -13,15 +13,12 @@ export type NitroFetchRequest =
   // eslint-disable-next-line @typescript-eslint/ban-types
   | (string & {});
 
+// TODO: re-enable Simplify
 export type MiddlewareOf<
   Route extends string,
   Method extends RouterMethod | "default"
 > = Method extends keyof InternalApi[MatchedRoutes<Route>]
-  ? Simplify<
-      Serialize<
-        Exclude<InternalApi[MatchedRoutes<Route>][Method], Error | void>
-      >
-    >
+  ? Exclude<Serialize<InternalApi[MatchedRoutes<Route>][Method]>, Error | void>
   : never;
 
 export type TypedInternalResponse<
