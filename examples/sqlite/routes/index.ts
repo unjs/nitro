1b115719afa1ce5ce79c332a24f59696b08f7c1e
Pooya Parsa <pooya@pi0.io>
2023-04-11 17:43:13 +0200

    simplify usage

--- examples/sqlite/routes/index.ts
+++ examples/sqlite/routes/index.ts
@@ -1,18 +1,11 @@
-export type BindParameters = unknown[] | Record<string, unknown>;
-
 export default defineEventHandler(async (event) => {
   const db = await useSQliteDB();
-
   await db.exec(
     "CREATE TABLE IF NOT EXISTS users (id TEXT PRIMARY KEY, firstName TEXT, lastName TEXT, email TEXT)"
   );
-
   const id = String(Math.round(Math.random() * 10000));
-
   await db.prepare("INSERT INTO users VALUES (?, 'John', 'Doe', '')").run(id);
-
   const row = await db.prepare("SELECT * FROM users WHERE id = ?").get(id);
-
   return {
     row,
   };
