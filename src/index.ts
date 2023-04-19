b395474367ae206e47c90f6ce64b5286b1697bf4
Pooya Parsa <pooya@pi0.io>
2023-04-19 20:59:51 +0200

    fix type export

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ export * from "./build";
 export * from "./nitro";
 export * from "./scan";
 export * from "./dev/server";
-export { LoadConfigOptions, loadOptions } from "./options";
 export * from "./types";
 export * from "./prerender";
 export * from "./preset";
+
+export { loadOptions } from "./options";
+export type { LoadConfigOptions } from "./options";
