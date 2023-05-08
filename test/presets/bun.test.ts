0a59419ec05a1a9f527419981a0054746b2a34bb
Pooya Parsa <pooya@pi0.io>
2023-05-08 13:54:27 +0200

    add tests and ci

---
+++ test/presets/bun.test.ts
@@ -0,0 +1,39 @@
+import { resolve } from "pathe";
+import { describe, it, expect } from "vitest";
+import { setupTest, testNitro } from "../tests";
+import { execa } from "execa";
+import { getRandomPort, waitForPort } from "get-port-please";
+import { fetch } from "ofetch";
+
+describe("nitro:preset:bun", async () => {
+  const ctx = await setupTest("bun");
+  testNitro(ctx, async () => {
+    const port = await getRandomPort();
+    process.env.PORT = String(port);
+    const p = execa("bun", [resolve(ctx.outDir, "server/index.mjs")], {
+      stdio: "inherit",
+    });
+    ctx.server = {
+      url: `http://127.0.0.1:${port}`,
+      close: () => p.kill(),
+    } as any;
+    await waitForPort(port);
+    return async ({ url, ...opts }) => {
+      const res = await ctx.fetch(url, opts);
+      return res;
+    };
+  });
+  it("should handle nested cached route rules", async () => {
+    const cached = await ctx.fetch("/rules/_/noncached/cached");
+    expect(cached.headers.get("etag")).toBeDefined();
+
+    const noncached = await ctx.fetch("/rules/_/noncached/noncached");
+    expect(noncached.headers.get("etag")).toBeNull();
+
+    const cached2 = await ctx.fetch("/rules/_/cached/cached");
+    expect(cached2.headers.get("etag")).toBeDefined();
+
+    const noncached2 = await ctx.fetch("/rules/_/cached/noncached");
+    expect(noncached2.headers.get("etag")).toBeNull();
+  });
+});
