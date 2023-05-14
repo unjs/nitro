daf87e22942383737f9cda618311dc70845c0962
Hebilicious <xsh4k3@gmail.com>
2023-05-14 16:57:02 +0700

    chore: remove useless binding in test

--- test/presets/cloudflare-module.test.ts
+++ test/presets/cloudflare-module.test.ts
@@ -1,8 +1,6 @@
-import { promises as fsp } from "node:fs";
-import { join, resolve } from "pathe";
+import { resolve } from "pathe";
 import { Miniflare } from "miniflare";
-import { describe, it, expect } from "vitest";
-import { Response as _Response } from "undici";
+import { describe } from "vitest";
 
 import { setupTest, testNitro } from "../tests";
 
@@ -14,16 +12,6 @@ describe("nitro:preset:cloudflare-module", async () => {
       modules: true,
       scriptPath: resolve(ctx.outDir, "server/index.mjs"),
       sitePath: resolve(ctx.outDir, "public"),
-      bindings: {
-        ASSETS: {
-          fetch: async (request) => {
-            const contents = await fsp.readFile(
-              join(ctx.outDir, new URL(request.url).pathname)
-            );
-            return new _Response(contents);
-          },
-        },
-      },
     });
 
     return async ({ url, headers, method, body }) => {
