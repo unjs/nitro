e559e3873179dd5ad16e51a0a21f9de7c658bd2c
魔王少年 <q267009886.tw@gmail.com>
2023-06-02 23:02:50 +0800

    feat: useStorage generic support

--- src/runtime/storage.ts
+++ src/runtime/storage.ts
@@ -1,7 +1,7 @@
-import type { Storage } from "unstorage";
+import type { Storage, StorageValue } from "unstorage";
 import { prefixStorage } from "unstorage";
 import { storage } from "#internal/nitro/virtual/storage";
 
-export function useStorage(base = ""): Storage {
+export function useStorage<T extends StorageValue = StorageValue>(base = ""): Storage<T> {
   return base ? prefixStorage(storage, base) : storage;
 }
