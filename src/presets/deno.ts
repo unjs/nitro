88b4e11854c6a84c764bf64329f8e50a946b03b8
Pooya Parsa <pooya@pi0.io>
2023-06-16 16:16:17 +0200

    fix(deno): support environment variables

--- src/presets/deno.ts
+++ src/presets/deno.ts
@@ -10,6 +10,9 @@ export const deno = defineNitroPreset({
     deploy:
       "cd ./ && deployctl deploy --project=<project_name> server/index.ts",
   },
+  unenv: {
+    polyfill: ["#internal/nitro/polyfill/deno-env"],
+  },
   rollupConfig: {
     preserveEntrySignatures: false,
     external: ["https://deno.land/std/http/server.ts"],
