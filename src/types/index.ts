c11b620301a338c03b61f3a8af47ae2c182d7220
Daniel Roe <daniel@roe.dev>
2023-06-21 20:05:21 +0100

    refactor: use unbuilt `types.d.ts`

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,4 +4,3 @@ export * from "./serialize";
 export * from "./nitro";
 export * from "./handler";
 export * from "./utils";
-export type * from "nitropack/dist/runtime/types";
--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    alias: {
-      "nitropack/dist/runtime": "./src/runtime",
-    },
     coverage: {
       reporter: ["text", "clover", "json"],
     },
