ba2772cc49738b0232c97f04cfa836b9682cdbbc
Daniel Roe <daniel@roe.dev>
2023-06-23 16:11:42 +0100

    fix: allow prerenderer to access static files

--- src/presets/nitro-prerender.ts
+++ src/presets/nitro-prerender.ts
@@ -2,6 +2,7 @@ import { defineNitroPreset } from "../preset";
 
 export const nitroPrerender = defineNitroPreset({
   extends: "node",
+  serveStatic: true,
   entry: "#internal/nitro/entries/nitro-prerenderer",
   output: {
     serverDir: "{{ buildDir }}/prerender",
