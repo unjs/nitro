486172e7da0bb91a5db3b26dc2a528142df7554b
Pooya Parsa <pooya@pi0.io>
2023-06-27 16:20:16 +0200

    revert playground

--- playground/routes/index.ts
+++ playground/routes/index.ts
@@ -1,8 +1,4 @@
+// eslint-disable-next-line require-await
 export default eventHandler(async (event) => {
-  const kvStorage = useStorage("data");
-  const counter = (Number(await kvStorage.getItem("counter")) || 0) + 1;
-  await kvStorage.setItem("counter", counter);
-  return {
-    counter,
-  };
+  return {};
 });
