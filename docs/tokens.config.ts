5151d7a58a2ae081cd6b376d226235384ee530c3
Serhii Bedrytskyi <bedrytskyi@gmail.com>
2023-06-29 19:16:56 +0300

    fix: primary color tokens; improve landing cards grid

--- docs/tokens.config.ts
+++ docs/tokens.config.ts
@@ -2,18 +2,18 @@ import { defineTheme } from 'pinceau'
 
 export default defineTheme({
   color: {
-    // https://www.tints.dev/primary/FB848E
     primary: {
-      50: "#FFF5F6",
-      100: "#FEEBED",
-      200: "#FED7DB",
-      300: "#FDBFC4",
-      400: "#FCA6AD",
+      50: "#FFF6F7",
+      100: "#FFEDEE",
+      200: "#FFDBDE",
+      300: "#FFBFC5",
+      400: "#FFA5AC",
       500: "#FB848E",
-      600: "#FA6672",
-      700: "#F83445",
-      800: "#DE0719",
-      900: "#A30512",
+      600: "#DD717A",
+      700: "#A34F57",
+      800: "#441F23",
+      900: "#1F0E10",
+
     }
   }
 })
