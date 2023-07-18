00b12c92903d572c7078d9b41982b28e683f929a
Aaron Dewes <aaron@runcitadel.space>
2023-07-18 07:47:10 +0200

    Treat all https:// modules as external

--- src/presets/deno-deploy.ts
+++ src/presets/deno-deploy.ts
@@ -16,14 +16,10 @@ export const denoDeploy = defineNitroPreset({
   },
   rollupConfig: {
     preserveEntrySignatures: false,
-    external: ["https://deno.land/std/http/server.ts"],
+    external: (id) => id.startsWith("https://"),
     output: {
       entryFileNames: "index.ts",
-      manualChunks: (id) => {
-        if (id !== "https://deno.land/std/http/server.ts") {
-          return "index";
-        }
-      },
+      manualChunks: (id) => "index",
       format: "esm",
     },
   },
