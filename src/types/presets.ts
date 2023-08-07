a949cb98f5984578b0aff5c10d66c4654647631b
Asher White <awhite@brochweb.com>
2023-08-07 13:30:26 -0400

    feat(cloudflare-pages): add config to customize generated `_routes.json` (#1312)
    Co-authored-by: Pooya Parsa <pooya@pi0.io>
    Co-authored-by: Heb <xsh4k3@gmail.com>

--- src/types/presets.ts
+++ src/types/presets.ts
@@ -1,3 +1,5 @@
+import type { CloudflarePagesRoutes } from "../presets/cloudflare-pages";
+
 /**
  * Vercel Build Output Configuration
  * @see https://vercel.com/docs/build-output-api/v3
@@ -90,5 +92,36 @@ export interface PresetOptions {
     regions?: string[];
 
     functions?: VercelServerlessFunctionConfig;
+  };
+  cloudflare: {
+    pages: {
+      /**
+       * Nitro will automatically generate a `_routes.json` that controls which files get served statically and
+       * which get served by the Worker. Using this config will override the automatic `_routes.json`. Or, if the
+       * `merge` options is set, it will merge the user-set routes with the auto-generated ones, giving priority
+       * to the user routes.
+       *
+       * @see https://developers.cloudflare.com/pages/platform/functions/routing/#functions-invocation-routes
+       *
+       * There are a maximum of 100 rules, and you must have at least one include rule. Wildcards are accepted.
+       *
+       * If any fields are unset, they default to:
+       *
+       * ```json
+       * {
+       *   "version": 1,
+       *   "include": ["/*"],
+       *   "exclude": []
+       * }
+       * ```
+       */
+      routes?: CloudflarePagesRoutes;
+      /**
+       * If set to `false`, nitro will disable the automatically generated `_routes.json` and instead use the user-set only ones.
+       *
+       * @default true
+       */
+      defaultRoutes?: boolean;
+    };
   };
 }
