370cd7e4c35623cc0bc2f7b3f15c603559718f0d
Pooya Parsa <pooya@pi0.io>
2023-08-07 16:15:35 +0200

    add more experimental notes

--- src/runtime/context.ts
+++ src/runtime/context.ts
@@ -15,7 +15,9 @@ export const nitroAsyncContext = getContext<NitroAsyncContext>("nitro-app", {
  *
  * Access to the current Nitro request event.
  *
- * @experimental Requires `experimental.asyncContext: true` config to work.
+ * @experimental
+ *  - Requires `experimental.asyncContext: true` config to work.
+ *  - Works in Node.js and limited runtimes only
  *
  */
 export function useEvent(): H3Event {
