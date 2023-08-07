e2f769ad741919c557034feaeb1486a2f0835e7b
Daniel Roe <daniel@roe.dev>
2023-08-07 22:26:16 +0100

    test: add failing tests

--- test/fixture/types.ts
+++ test/fixture/types.ts
@@ -13,6 +13,19 @@ describe("API routes", () => {
   // eslint-disable-next-line @typescript-eslint/no-inferrable-types
   const dynamicString: string = "";
 
+  it("requires correct options for typed routes", () => {
+    // @ts-expect-error should be a POST request
+    $fetch("/api/upload");
+    // @ts-expect-error `query.id` is required
+    $fetch("/typed-routes");
+    // @ts-expect-error `query.id` is required
+    $fetch("/typed-routes", {});
+    // @ts-expect-error `query.id` should be a string
+    $fetch("/typed-routes", { query: { id: 42 } });
+
+    expectTypeOf($fetch("/typed-routes", { query: { id: 'string' } })).toEqualTypeOf<Promise<string>>();
+  });
+
   it("generates types for middleware, unknown and manual typed routes", () => {
     expectTypeOf($fetch("/")).toEqualTypeOf<Promise<unknown>>(); // middleware
     expectTypeOf($fetch("/api/unknown")).toEqualTypeOf<Promise<unknown>>();
