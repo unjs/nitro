fd9c76bb519731d4abe30c3212b3fb3936e319ee
Pooya Parsa <pooya@pi0.io>
2023-08-09 18:27:34 +0200

    fix(bun): disable require for now

--- src/presets/bun.ts
+++ src/presets/bun.ts
@@ -1,11 +1,10 @@
-import { resolvePathSync } from "mlly";
 import { defineNitroPreset } from "../preset";
 
 export const bun = defineNitroPreset({
   extends: "node-server",
   entry: "#internal/nitro/entries/bun",
   // https://bun.sh/docs/runtime/modules#resolution
-  exportConditions: ["bun", "worker", "node", "require", "import", "default"],
+  exportConditions: ["bun", "worker", "node", "import", "default"],
   commands: {
     preview: "bun run ./server/index.mjs",
   },
