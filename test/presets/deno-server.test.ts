71828d1516245dc56512d4c3d1094749bef7b9c0
Daniel Roe <daniel@roe.dev>
2023-08-16 12:08:15 +0200

    chore: lint test files as well

--- test/presets/deno-server.test.ts
+++ test/presets/deno-server.test.ts
@@ -8,28 +8,31 @@ const hasDeno =
   execaCommandSync("deno --version", { stdio: "ignore", reject: false })
     .exitCode === 0;
 
-describe.runIf(hasDeno && false /* h3 latest needed */)("nitro:preset:deno-server", async () => {
-  const ctx = await setupTest("deno-server");
-  testNitro(ctx, async () => {
-    const port = await getRandomPort();
-    const p = execa(
-      "deno",
-      ["task", "--config", resolve(ctx.outDir, "deno.json"), "start"],
-      {
-        stdio: "inherit",
-        env: {
-          PORT: String(port),
-        },
-      }
-    );
-    ctx.server = {
-      url: `http://127.0.0.1:${port}`,
-      close: () => p.kill(),
-    } as any;
-    await waitForPort(port, { delay: 1000, retries: 20 });
-    return async ({ url, ...opts }) => {
-      const res = await ctx.fetch(url, opts);
-      return res;
-    };
-  });
-});
+describe.runIf(hasDeno && false /* h3 latest needed */)(
+  "nitro:preset:deno-server",
+  async () => {
+    const ctx = await setupTest("deno-server");
+    testNitro(ctx, async () => {
+      const port = await getRandomPort();
+      const p = execa(
+        "deno",
+        ["task", "--config", resolve(ctx.outDir, "deno.json"), "start"],
+        {
+          stdio: "inherit",
+          env: {
+            PORT: String(port),
+          },
+        }
+      );
+      ctx.server = {
+        url: `http://127.0.0.1:${port}`,
+        close: () => p.kill(),
+      } as any;
+      await waitForPort(port, { delay: 1000, retries: 20 });
+      return async ({ url, ...opts }) => {
+        const res = await ctx.fetch(url, opts);
+        return res;
+      };
+    });
+  }
+);
