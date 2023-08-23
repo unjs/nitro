8a6ed77b85aec8059bff4d575ea02c91faae42e5
Pooya Parsa <pooya@pi0.io>
2023-08-23 04:34:22 +0200

    test(netlify): run after

--- test/presets/netlify.test.ts
+++ test/presets/netlify.test.ts
@@ -13,77 +13,82 @@ describe("nitro:preset:netlify", async () => {
       },
     },
   });
-  testNitro(ctx, async () => {
-    const { handler } = (await import(
-      resolve(ctx.outDir, "server/server.mjs")
-    )) as { handler: Handler };
-    return async ({ url: rawRelativeUrl, headers, method, body }) => {
-      // creating new URL object to parse query easier
-      const url = new URL(`https://example.com${rawRelativeUrl}`);
-      const queryStringParameters = Object.fromEntries(
-        url.searchParams.entries()
-      );
-      const event: Partial<APIGatewayEvent> = {
-        resource: "/my/path",
-        path: url.pathname,
-        headers: headers || {},
-        httpMethod: method || "GET",
-        queryStringParameters,
-        body: body || "",
+  testNitro(
+    ctx,
+    async () => {
+      const { handler } = (await import(
+        resolve(ctx.outDir, "server/server.mjs")
+      )) as { handler: Handler };
+      return async ({ url: rawRelativeUrl, headers, method, body }) => {
+        // creating new URL object to parse query easier
+        const url = new URL(`https://example.com${rawRelativeUrl}`);
+        const queryStringParameters = Object.fromEntries(
+          url.searchParams.entries()
+        );
+        const event: Partial<APIGatewayEvent> = {
+          resource: "/my/path",
+          path: url.pathname,
+          headers: headers || {},
+          httpMethod: method || "GET",
+          queryStringParameters,
+          body: body || "",
+        };
+        const res = await handler(event, {} as any, () => {});
+        const resHeaders = { ...res.headers, ...res.multiValueHeaders };
+        return {
+          data: destr(res.body),
+          status: res.statusCode,
+          headers: resHeaders,
+        };
       };
-      const res = await handler(event, {} as any, () => {});
-      const resHeaders = { ...res.headers, ...res.multiValueHeaders };
-      return {
-        data: destr(res.body),
-        status: res.statusCode,
-        headers: resHeaders,
-      };
-    };
-  });
-  it("should add route rules - redirects", async () => {
-    const redirects = await fsp.readFile(
-      resolve(ctx.rootDir, "dist/_redirects"),
-      "utf8"
-    );
-    /* eslint-disable no-tabs */
-    expect(redirects).toMatchInlineSnapshot(`
-      "/rules/nested/override	/other	302
-      /rules/redirect/obj	https://nitro.unjs.io/	301
-      /rules/nested/*	/base	302
-      /rules/redirect	/base	302
-      /rules/_/cached/noncached	/.netlify/functions/server 200
-      /rules/_/noncached/cached	/.netlify/builders/server 200
-      /rules/_/cached/*	/.netlify/builders/server 200
-      /rules/_/noncached/*	/.netlify/functions/server 200
-      /rules/swr-ttl/*	/.netlify/builders/server 200
-      /rules/swr/*	/.netlify/builders/server 200
-      /rules/isr-ttl/*	/.netlify/builders/server 200
-      /rules/isr/*	/.netlify/builders/server 200
-      /rules/dynamic	/.netlify/functions/server 200
-      /* /.netlify/functions/server 200"
-    `);
-    /* eslint-enable no-tabs */
-  });
-  it("should add route rules - headers", async () => {
-    const headers = await fsp.readFile(
-      resolve(ctx.rootDir, "dist/_headers"),
-      "utf8"
-    );
-    /* eslint-disable no-tabs */
-    expect(headers).toMatchInlineSnapshot(`
-      "/rules/headers
-        cache-control: s-maxage=60
-      /rules/cors
-        access-control-allow-origin: *
-        access-control-allow-methods: GET
-        access-control-allow-headers: *
-        access-control-max-age: 0
-      /rules/nested/*
-        x-test: test
-      /build/*
-        cache-control: public, max-age=3600, immutable
-      "
-    `);
-    /* eslint-enable no-tabs */
-  });
+    },
+    () => {
+      it("should add route rules - redirects", async () => {
+        const redirects = await fsp.readFile(
+          resolve(ctx.rootDir, "dist/_redirects"),
+          "utf8"
+        );
+        /* eslint-disable no-tabs */
+        expect(redirects).toMatchInlineSnapshot(`
+        "/rules/nested/override	/other	302
+        /rules/redirect/obj	https://nitro.unjs.io/	301
+        /rules/nested/*	/base	302
+        /rules/redirect	/base	302
+        /rules/_/cached/noncached	/.netlify/functions/server 200
+        /rules/_/noncached/cached	/.netlify/builders/server 200
+        /rules/_/cached/*	/.netlify/builders/server 200
+        /rules/_/noncached/*	/.netlify/functions/server 200
+        /rules/swr-ttl/*	/.netlify/builders/server 200
+        /rules/swr/*	/.netlify/builders/server 200
+        /rules/isr-ttl/*	/.netlify/builders/server 200
+        /rules/isr/*	/.netlify/builders/server 200
+        /rules/dynamic	/.netlify/functions/server 200
+        /* /.netlify/functions/server 200"
+      `);
+        /* eslint-enable no-tabs */
+      });
+      it("should add route rules - headers", async () => {
+        const headers = await fsp.readFile(
+          resolve(ctx.rootDir, "dist/_headers"),
+          "utf8"
+        );
+        /* eslint-disable no-tabs */
+        expect(headers).toMatchInlineSnapshot(`
+        "/rules/headers
+          cache-control: s-maxage=60
+        /rules/cors
+          access-control-allow-origin: *
+          access-control-allow-methods: GET
+          access-control-allow-headers: *
+          access-control-max-age: 0
+        /rules/nested/*
+          x-test: test
+        /build/*
+          cache-control: public, max-age=3600, immutable
+        "
+      `);
+        /* eslint-enable no-tabs */
+      });
+    }
+  );
 });
