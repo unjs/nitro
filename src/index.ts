c32c51ade897c001156ff4dea6e9ddeeb33e2940
Pooya Parsa <pooya@pi0.io>
2023-10-28 15:14:09 +0200

    avoid exporting `defeineNitroModule`
    it is globally available. encouraging import from main package can lead to later hoist issues

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ export * from "./dev/server";
 export * from "./types";
 export * from "./prerender";
 export * from "./preset";
-export * from "./module";
 export * from "./deps";
 
 export { loadOptions } from "./options";
