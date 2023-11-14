55bb3b41b7fec8b632012158d7fd31d4c57fb3bd
Pooya Parsa <pyapar@gmail.com>
2023-11-14 03:50:02 +0100

    refactor: split preset types (#1910)

---
+++ src/types/presets/index.ts
@@ -0,0 +1,11 @@
+import { AzureOptions } from "./azure";
+import { CloudflareOptions } from "./cloudflare";
+import { FirebaseOptions } from "./firebase";
+import { VercelOptions } from "./vercel";
+
+export interface PresetOptions {
+  azure: AzureOptions;
+  cloudflare: CloudflareOptions;
+  firebase: FirebaseOptions;
+  vercel: VercelOptions;
+}
