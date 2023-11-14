d675cf4477b4ca6359ad46faa6e3605e82bda996
Pooya Parsa <pooya@pi0.io>
2023-11-14 03:48:08 +0100

    refactor: split preset types

---
+++ src/types/presets/vercel.ts
@@ -0,0 +1,93 @@
+/**
+ * Vercel Build Output Configuration
+ * @see https://vercel.com/docs/build-output-api/v3
+ */
+export interface VercelBuildConfigV3 {
+  version: 3;
+  routes?: (
+    | {
+        src: string;
+        headers: {
+          "cache-control": string;
+        };
+        continue: boolean;
+      }
+    | {
+        handle: string;
+      }
+    | {
+        src: string;
+        dest: string;
+      }
+  )[];
+  images?: {
+    sizes: number[];
+    domains: string[];
+    remotePatterns?: {
+      protocol?: "http" | "https";
+      hostname: string;
+      port?: string;
+      pathname?: string;
+    }[];
+    minimumCacheTTL?: number;
+    formats?: ("image/avif" | "image/webp")[];
+    dangerouslyAllowSVG?: boolean;
+    contentSecurityPolicy?: string;
+  };
+  wildcard?: Array<{
+    domain: string;
+    value: string;
+  }>;
+  overrides?: Record<
+    string,
+    {
+      path?: string;
+      contentType?: string;
+    }
+  >;
+  cache?: string[];
+  bypassToken?: string;
+  crons?: {
+    path: string;
+    schedule: string;
+  }[];
+}
+
+/**
+ * https://vercel.com/docs/build-output-api/v3/primitives#serverless-function-configuration
+ */
+export interface VercelServerlessFunctionConfig {
+  /**
+   * Amount of memory (RAM in MB) that will be allocated to the Serverless Function.
+   */
+  memory?: number;
+
+  /**
+   * Maximum execution duration (in seconds) that will be allowed for the Serverless Function.
+   */
+  maxDuration?: number;
+
+  /**
+   * True if a custom runtime has support for Lambda runtime wrappers.
+   */
+  supportsWrapper?: boolean;
+
+  /**
+   * When true, the Serverless Function will stream the response to the client.
+   */
+  supportsResponseStreaming?: boolean;
+
+  [key: string]: unknown;
+}
+
+export interface VercelOptions {
+  config: VercelBuildConfigV3;
+
+  /**
+   * If you are using `vercel-edge`, you can specify the region(s) for your edge function.
+   * @see https://vercel.com/docs/concepts/functions/edge-functions#edge-function-regions
+   */
+  regions?: string[];
+
+  functions?: VercelServerlessFunctionConfig;
+}
