9a1da3d50198b8353a7e75f071f4b910f1a53c61
Pooya Parsa <pooya@pi0.io>
2023-11-14 03:49:37 +0100

    small refactors

--- src/types/presets/cloudflare.ts
+++ src/types/presets/cloudflare.ts
@@ -1,4 +1,16 @@
-import type { CloudflarePagesRoutes } from "../../presets/cloudflare-pages";
+/**
+ * https://developers.cloudflare.com/pages/platform/functions/routing/#functions-invocation-routes
+ */
+export interface CloudflarePagesRoutes {
+  /** Defines the version of the schema. Currently there is only one version of the schema (version 1), however, we may add more in the future and aim to be backwards compatible. */
+  version?: 1;
+
+  /** Defines routes that will be invoked by Functions. Accepts wildcard behavior. */
+  include?: string[];
+
+  /** Defines routes that will not be invoked by Functions. Accepts wildcard behavior. `exclude` always take priority over `include`. */
+  exclude?: string[];
+}
 
 export interface CloudflareOptions {
   pages: {
