103328b84457d944a31dff7df091b0b826df7644
Ayo <ramon.aycojr@gmail.com>
2023-11-27 18:19:13 +0100

    fix: undo test config

--- playground/nitro.config.ts
+++ playground/nitro.config.ts
@@ -1,11 +1,1 @@
-export default defineNitroConfig({
-    framework: {
-        name: 'McFly',
-        version: '0.13.3',
-        showBuildSuccess: 'verbose'
-    },
-    publicAssets: [{
-        dir: 'something'
-    }]
-});
-
+export default defineNitroConfig({});
