079cf48a08efa743a93209fbdeff1b65836b2556
Pooya Parsa <pooya@pi0.io>
2023-11-28 03:01:28 +0100

    enable windows test for wasm

--- test/tests.ts
+++ test/tests.ts
@@ -8,7 +8,6 @@ import { expect, it, afterAll, beforeAll, describe } from "vitest";
 import { fileURLToPath } from "mlly";
 import { joinURL } from "ufo";
 import { defu } from "defu";
-import { isWindows } from "std-env";
 import * as _nitro from "../src";
 import type { Nitro } from "../src";
 
@@ -632,7 +631,7 @@ export function testNitro(
   });
 
   describe("wasm", () => {
-    it.skipIf(ctx.isWorker || ctx.preset === "deno-server" || isWindows)(
+    it.skipIf(ctx.isWorker || ctx.preset === "deno-server")(
       "dynamic import wasm",
       async () => {
         expect((await callHandler({ url: "/wasm/dynamic" })).data).toBe(
