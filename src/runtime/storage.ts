8fbe75cb23d5728c73370faeb0b29d067048ed78
Michael Brevard <yonshi29@gmail.com>
2023-12-10 12:11:02 +0200

    feat(storage): provide type safety for useStorage

--- src/runtime/storage.ts
+++ src/runtime/storage.ts
@@ -1,9 +1,10 @@
 import type { Storage, StorageValue } from "unstorage";
 import { prefixStorage } from "unstorage";
 import { storage } from "#internal/nitro/virtual/storage";
-
+import {mountPoints} from "../../utils/storage"
+import type {StorageKeys} from "../../types/utils"
 export function useStorage<T extends StorageValue = StorageValue>(
-  base = ""
+  base = StorageKeys<typeof mountPoints> | undefined
 ): Storage<T> {
   return base ? prefixStorage<T>(storage, base) : storage;
 }
