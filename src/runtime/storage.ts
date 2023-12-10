cad5dad29285b3b9a10f2e47dc68dfa414b25eef
Michael Brevard <yonshi29@gmail.com>
2023-12-10 12:31:47 +0200

    refactor(storage): convert to objects

--- src/runtime/storage.ts
+++ src/runtime/storage.ts
@@ -1,7 +1,6 @@
 import type { Storage, StorageValue } from "unstorage";
 import { prefixStorage } from "unstorage";
 import { mountPoints } from "../../utils/storage";
-import type { StorageKeys } from "../../types/utils";
 import { storage } from "#internal/nitro/virtual/storage";
 
 export function useStorage<T extends StorageValue = StorageValue>(
