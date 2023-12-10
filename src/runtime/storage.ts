421ea80d1125376b63ff5d8804e68343d34009e7
Michael Brevard <yonshi29@gmail.com>
2023-12-10 12:18:11 +0200

    fix: base

--- src/runtime/storage.ts
+++ src/runtime/storage.ts
@@ -5,7 +5,7 @@ import type { StorageKeys } from "../../types/utils";
 import { storage } from "#internal/nitro/virtual/storage";
 
 export function useStorage<T extends StorageValue = StorageValue>(
-  base = StorageKeys<typeof mountPoints> | undefined
+  base extends StorageKeys<typeof mountPoints> | undefined ="" 
 ): Storage<T> {
   return base ? prefixStorage<T>(storage, base) : storage;
 }
