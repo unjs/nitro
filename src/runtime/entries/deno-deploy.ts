aacd50ff3020d6c216cd21000669af119d96e514
jasonleong <jasonliangbiz@gmail.com>
2023-12-25 11:08:32 +0800

    Update deno-deploy.ts
    add x-forwarded-for header

--- src/runtime/entries/deno-deploy.ts
+++ src/runtime/entries/deno-deploy.ts
@@ -3,12 +3,14 @@ import "#internal/nitro/virtual/polyfill";
 import { nitroApp } from "../app";
 
 // @ts-expect-error unknown global Deno
-Deno.serve((request: Request) => {
-  return handleRequest(request);
+Deno.serve((request,info) => {
+  return handleRequest(request,info);
 });
 
-async function handleRequest(request: Request) {
+async function handleRequest(request: Request,info) {
   const url = new URL(request.url);
+  const headers = new Headers(request.headers);
+  headers.set("x-forwarded-for", info.remoteAddr.hostname);//add x-forwarded-for header.
 
   // https://deno.land/api?s=Body
   let body;
