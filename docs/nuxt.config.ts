ef0355e976e561f80f67f49b1397388bf18f6828
barbapapazes <e.soubiran25@gmail.com>
2024-01-17 00:18:10 +0100

    docs: add seo

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
   extends: "@nuxt-themes/docus",
-  modules: ["@nuxtjs/plausible"],
+  modules: ["@nuxtjs/plausible", "@nuxtjs/seo"],
   routeRules: {
     "/deploy/node": { redirect: "/deploy/runtimes/node" },
   },
@@ -9,4 +9,22 @@ export default defineNuxtConfig({
       failOnError: false,
     },
   },
+  site: {
+    url: 'https://nitro.unjs.io',
+    name: 'Nitro',
+    description: 'Nitro is an open source TypeScript framework to build ultra-fast web servers. The open engine powering Nuxt and open to everyone.',
+    defaultLocale: 'en',
+  },
+  schemaOrg: {
+    identity: 'Organization',
+    name: "Nitro",
+    url: 'https://nitro.unjs.io',
+    logo: 'https://nitro.unjs.io/nitro.png',
+  },
+  seoExperiments: {
+    enabled: false
+  },
+  linkChecker: {
+    enabled: false
+  }
 });
