1ca6361f43cad8417e4ad1810cfd8adce408a3b8
Alexander Lichter <github@lichter.io>
2024-02-29 11:38:33 +0100

    fix: only set x-forwarded-for when exists

--- src/dev/server.ts
+++ src/dev/server.ts
@@ -197,7 +197,10 @@ export function createDevServer(nitro: Nitro): NitroDevServer {
   proxy.proxy.on("proxyReq", (proxyReq, req) => {
     // TODO: Use httpxy to set these headers
     if (!proxyReq.hasHeader("x-forwarded-for")) {
-      proxyReq.appendHeader("x-forwarded-for", req.socket.remoteAddress);
+      const address = req.socket.remoteAddress
+      if(address) {
+        proxyReq.appendHeader("x-forwarded-for", address);
+      } 
     }
     if (!proxyReq.hasHeader("x-forwarded-port")) {
       const localPort = req?.socket?.localPort;
