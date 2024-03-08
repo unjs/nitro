7a65d1ef4a777b95cc0a3fb08f2ae7e62e696a51
Pooya Parsa <pooya@pi0.io>
2024-03-08 22:00:28 +0100

    chore: increase test timeout

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    testTimeout: 10_000,
     coverage: {
       reporter: ["text", "clover", "json"],
     },
