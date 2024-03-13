0b4b5c26920cebb7954bdb2c636c975d15528f0d
Daniel Roe <daniel@roe.dev>
2024-03-13 16:16:02 -0700

    fix: handle path negations when scanning public assets (#2250)

--- test/tests.ts
+++ test/tests.ts
@@ -539,6 +539,15 @@ export function testNitro(
         expect((await callHandler({ url: "/favicon.ico" })).status).toBe(200);
       }
     );
+
+    it.skipIf(ctx.isWorker || ctx.isDev)(
+      "public files can be un-ignored with patterns",
+      async () => {
+        expect((await callHandler({ url: "/_unignored.txt" })).status).toBe(
+          200
+        );
+      }
+    );
   });
 
   describe("headers", () => {
