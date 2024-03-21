5184e45e1a5aad47e4fc560fa6f0700b97ed0221
Daniel Roe <daniel@roe.dev>
2024-03-21 16:51:12 +0000

    fix(prerender): call `nitroApp` close hook when done prerendering (#2287)

--- src/runtime/entries/nitro-prerenderer.ts
+++ src/runtime/entries/nitro-prerenderer.ts
@@ -3,6 +3,7 @@ import { nitroApp } from "../app";
 import { trapUnhandledNodeErrors } from "../utils";
 
 export const localFetch = nitroApp.localFetch;
+export const closePrerenderer = () => nitroApp.hooks.callHook("close");
 
 // Trap unhandled errors
 trapUnhandledNodeErrors();
