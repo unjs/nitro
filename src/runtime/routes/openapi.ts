7f0c08d0e58e2689daa2eb722825d40572483f94
Hans Pagel <mail@hanspagel.com>
2024-03-22 12:22:48 -0700

    chore: add url description again

--- src/runtime/routes/openapi.ts
+++ src/runtime/routes/openapi.ts
@@ -25,7 +25,7 @@ export default eventHandler((event) => {
     servers: [
       {
         url,
-        description: null,
+        description: "Local Development Server",
         variables: {},
       },
     ],
