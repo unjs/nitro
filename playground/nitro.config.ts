72c9a289e79ff3a54198097e35b3b304dab89816
Pooya Parsa <pooya@pi0.io>
2024-04-03 14:26:50 +0200

    move config to fixture

--- playground/nitro.config.ts
+++ playground/nitro.config.ts
@@ -1,17 +1,1 @@
-export default defineNitroConfig({
-  experimental: {
-    openAPI: true,
-  },
-  openAPI: {
-    meta: {
-      title: 'Foobar',
-      description: 'Foobar API',
-      version: '2.0',
-    },
-    ui: {
-      scalar: {
-        theme: 'purple'
-      }
-    }
-  },
-});
+export default defineNitroConfig({});
