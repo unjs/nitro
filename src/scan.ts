0cba4579c4cfcada78769c93f65deebaa73abb72
Pooya Parsa <pooya@pi0.io>
2024-04-09 19:46:14 +0200

    revert unnecessary await

--- src/scan.ts
+++ src/scan.ts
@@ -52,36 +52,34 @@ export async function scanServerRoutes(
   prefix = "/"
 ) {
   const files = await scanFiles(nitro, dir);
-  return await Promise.all(
-    files.map((file) => {
-      let route = file.path
-        .replace(/\.[A-Za-z]+$/, "")
-        .replace(/\[\.{3}]/g, "**")
-        .replace(/\[\.{3}(\w+)]/g, "**:$1")
-        .replace(/\[(\w+)]/g, ":$1");
-      route = withLeadingSlash(withoutTrailingSlash(withBase(route, prefix)));
+  return files.map((file) => {
+    let route = file.path
+      .replace(/\.[A-Za-z]+$/, "")
+      .replace(/\[\.{3}]/g, "**")
+      .replace(/\[\.{3}(\w+)]/g, "**:$1")
+      .replace(/\[(\w+)]/g, ":$1");
+    route = withLeadingSlash(withoutTrailingSlash(withBase(route, prefix)));
 
-      const suffixMatch = route.match(suffixRegex);
-      let method: MatchedMethdSuffix | undefined;
-      let env: MatchedEnvSuffix | undefined;
-      if (suffixMatch) {
-        route = route.slice(0, Math.max(0, suffixMatch.index));
-        method = suffixMatch[1] as MatchedMethdSuffix;
-        env = suffixMatch[3] as MatchedEnvSuffix;
-      }
+    const suffixMatch = route.match(suffixRegex);
+    let method: MatchedMethdSuffix | undefined;
+    let env: MatchedEnvSuffix | undefined;
+    if (suffixMatch) {
+      route = route.slice(0, Math.max(0, suffixMatch.index));
+      method = suffixMatch[1] as MatchedMethdSuffix;
+      env = suffixMatch[3] as MatchedEnvSuffix;
+    }
 
-      route = route.replace(/\/index$/, "") || "/";
+    route = route.replace(/\/index$/, "") || "/";
 
-      return {
-        handler: file.fullPath,
-        lazy: true,
-        middleware: false,
-        route,
-        method,
-        env,
-      };
-    })
-  );
+    return {
+      handler: file.fullPath,
+      lazy: true,
+      middleware: false,
+      route,
+      method,
+      env,
+    };
+  });
 }
 
 export async function scanPlugins(nitro: Nitro) {
