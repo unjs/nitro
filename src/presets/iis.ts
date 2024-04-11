e6b91a681b03e0436157c352e4ff1a7c60a2fd42
Pooya Parsa <pyapar@gmail.com>
2024-04-11 15:37:25 +0200

    Apply suggestions from code review

--- src/presets/iis.ts
+++ src/presets/iis.ts
@@ -117,7 +117,7 @@ async function iisnodeXmlTemplate(nitro: Nitro) {
       await parseXmlDoc(fileString);
 
     if (nitro.options.iis?.mergeConfig && !nitro.options.iis.overrideConfig) {
-      return buildNewXmlDoc(defu(originalWebConfig, fileWebConfig));
+      return buildNewXmlDoc(defu(fileWebConfig, originalWebConfig));
     } else if (nitro.options.iis?.overrideConfig) {
       return buildNewXmlDoc({ ...fileWebConfig });
     }
@@ -150,7 +150,7 @@ async function iisXmlTemplate(nitro: Nitro) {
       await parseXmlDoc(fileString);
 
     if (nitro.options.iis?.mergeConfig && !nitro.options.iis.overrideConfig) {
-      return buildNewXmlDoc(defu(originalWebConfig, fileWebConfig));
+      return buildNewXmlDoc(defu(fileWebConfig, originalWebConfig));
     } else if (nitro.options.iis?.overrideConfig) {
       return buildNewXmlDoc({ ...fileWebConfig });
     }
