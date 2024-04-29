4f2533c9ada90954b2496fa4a0566d00f5ffd0f0
Daniel Roe <daniel@roe.dev>
2024-04-29 20:02:59 +0100

    fix: set `compilerOptions.noEmit` to avoid `tsconfig.json` warning (#2402)

--- src/build.ts
+++ src/build.ts
@@ -284,6 +284,7 @@ declare module 'nitropack' {
       compilerOptions: {
         forceConsistentCasingInFileNames: true,
         strict: nitro.options.typescript.strict,
+        noEmit: true,
         target: "ESNext",
         module: "ESNext",
         moduleResolution:
