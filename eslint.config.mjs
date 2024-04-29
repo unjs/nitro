78ec24d1b487a826e6c6a284f24e49a0e5a03f5a
Pooya Parsa <pooya@pi0.io>
2024-04-29 13:18:35 +0200

    chore: update eslint preset

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,15 +7,13 @@ export default unjs(
       "**/.nitro",
       "**/.netlify",
       "**/.nuxt"
-    ]
-  },
-  {
+    ],
     rules: {
       "unicorn/no-null": 0,
       "no-undef": 0,
       "@typescript-eslint/no-unused-vars": 0,
       "unicorn/filename-case": 0,
-      "unicorn/consistent-function-scoping": 0
-    }
+      "unicorn/consistent-function-scoping": 0,
+    },
   }
 );
