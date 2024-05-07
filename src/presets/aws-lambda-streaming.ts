62345ddf4fc01eb3d5dd0d6ba2c1ccfa334a04c6
Pooya Parsa <pooya@pi0.io>
2024-05-07 15:10:02 +0200

    chore: extend `aws-lambda` base

--- src/presets/aws-lambda-streaming.ts
+++ src/presets/aws-lambda-streaming.ts
@@ -1,5 +1,6 @@
 import { defineNitroPreset } from "../preset";
 
 export const awsLambdaStreaming = defineNitroPreset({
+  extends: "aws-lambda",
   entry: "#internal/nitro/entries/aws-lambda-streaming",
 });
