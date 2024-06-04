f013df9770352202d02ac3229f381ce50eef88f1
Pooya Parsa <pooya@pi0.io>
2024-06-04 18:54:59 +0200

    fix types

--- src/core/utils/error.ts
+++ src/core/utils/error.ts
@@ -1,17 +1,26 @@
-export function formatRollupError(_error: RollupError | OnResolveResult) {
+import type esbuild from "esbuild";
+import { isAbsolute, relative } from "pathe";
+import type rollup from "rollup";
+
+export function formatRollupError(
+  _error: rollup.RollupError | esbuild.OnResolveResult
+) {
   try {
     const logs: string[] = [_error.toString()];
-    const errors = (_error as any)?.errors || [_error as RollupError];
+    const errors = (_error as any)?.errors || [_error as rollup.RollupError];
     for (const error of errors) {
-      const id = (error as any).path || error.id || (_error as RollupError).id;
+      const id =
+        (error as any).path || error.id || (_error as rollup.RollupError).id;
       let path = isAbsolute(id) ? relative(process.cwd(), id) : id;
       const location =
-        (error as RollupError).loc || (error as PartialMessage).location;
+        (error as rollup.RollupError).loc ||
+        (error as esbuild.PartialMessage).location;
       if (location) {
         path += `:${location.line}:${location.column}`;
       }
       const text =
-        (error as PartialMessage).text || (error as RollupError).frame;
+        (error as esbuild.PartialMessage).text ||
+        (error as rollup.RollupError).frame;
       logs.push(
         `Rollup error while processing \`${path}\`` + text ? "\n\n" + text : ""
       );
