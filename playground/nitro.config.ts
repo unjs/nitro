234fd6c45083da6f8bd029a264485960775c0912
barbapapazes <e.soubiran25@gmail.com>
2024-06-10 22:03:42 +0200

    chore(playground): does not overwrite generate tsconfig paths

--- playground/nitro.config.ts
+++ playground/nitro.config.ts
@@ -1,3 +1,17 @@
 import { defineNitroConfig } from "nitropack/config";
 
-export default defineNitroConfig({});
+export default defineNitroConfig({
+  typescript: {
+    // Because the TSConfig extends properties is not a merge but overwrite keys, we loose the paths from the NitroPack TSConfig.
+    tsConfig: {
+      compilerOptions: {
+        paths: {
+          nitropack: ["../../../src/index"],
+          "nitropack/config": ["../../../src/config"],
+          "#internal/nitro": ["../../../src/runtime/index"],
+          "#internal/nitro/*": ["../../../src/runtime/*"],
+        },
+      },
+    },
+  },
+});
