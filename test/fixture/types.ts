54b37b890b848ac944ccf05a25580b4e3d1554ba
Daniel Roe <daniel@roe.dev>
2024-06-20 20:50:07 +0100

    test: add failing test for omitted method fail

--- test/fixture/types.ts
+++ test/fixture/types.ts
@@ -183,6 +183,9 @@ describe("API routes", () => {
   });
 
   it("generates the correct type depending on the method used", () => {
+    expectTypeOf($fetch("/api/methods")).toEqualTypeOf<
+      Promise<"Index get">
+    >();
     expectTypeOf($fetch("/api/methods", { method: "get" })).toEqualTypeOf<
       Promise<"Index get">
     >();
