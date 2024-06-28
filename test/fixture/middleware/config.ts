3c11ca87e9383e975b4baab925b998131639d30d
Pooya Parsa <pooya@pi0.io>
2024-06-28 14:51:15 +0200

    remove log

--- test/fixture/middleware/config.ts
+++ test/fixture/middleware/config.ts
@@ -1,7 +1,6 @@
 process.env.NITRO_DYNAMIC = "from-env";
 
 export default eventHandler((event) => {
-  console.log(">>", event.path);
   const appConfig = useAppConfig(event);
   appConfig.dynamic = "from-middleware";
 });
