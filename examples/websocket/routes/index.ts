9415e0f1faccbba7f3c0a1703343b68e580591a7
iiio2 <gomexxxx71@gmail.com>
2024-07-02 19:40:01 +0600

    chore: update websocket example

--- examples/websocket/routes/index.ts
+++ examples/websocket/routes/index.ts
@@ -1,5 +1,7 @@
-export default defineEventHandler(() => {
-  return $fetch(
+export default defineEventHandler(async () => {
+  const data =  await $fetch(
     "https://raw.githubusercontent.com/unjs/crossws/main/examples/h3/public/index.html"
   );
+
+  return data 
 });
