52e231f72a5ea330f1bc9fe813c1e2a4f3a4c2ce
Pooya Parsa <pyapar@gmail.com>
2024-10-10 15:28:20 +0200

    Update src/runtime/internal/cache.ts
    Co-authored-by: Sébastien Chopin <seb@nuxt.com>

--- src/runtime/internal/cache.ts
+++ src/runtime/internal/cache.ts
@@ -114,7 +114,7 @@ export function defineCachedFunction<T, ArgsT extends unknown[] = any[]>(
         delete pending[key];
         if (validate(entry) !== false) {
           let setOpts: TransactionOptions | undefined;
-          if (opts.maxAge && !opts.swr /* TODO: respect respectstaleMaxAge */) {
+          if (opts.maxAge && !opts.swr /* TODO: respect staleMaxAge */) {
             setOpts = { ttl: opts.maxAge };
           }
           const promise = useStorage()
