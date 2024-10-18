unjs/nitro

commit d7882187f0fb997eb16717c8e2c569cd0d226593

feat(renderer): add `render:before` hook (#2770)

src/runtime/internal/renderer.ts
@@ -7,24 +7,33 @@ import {
   setResponseHeaders,
   setResponseStatus,
 } from "h3";
-import type { RenderHandler } from "nitropack/types";
+import type { RenderHandler, RenderContext } from "nitropack/types";
 import { useNitroApp } from "./app";
 import { useRuntimeConfig } from "./config";
 
-export function defineRenderHandler(handler: RenderHandler) {
+export function defineRenderHandler(render: RenderHandler) {
   const runtimeConfig = useRuntimeConfig();
   return eventHandler(async (event) => {
-    // TODO: Use serve-placeholder
-    if (event.path === `${runtimeConfig.app.baseURL}favicon.ico`) {
-      setResponseHeader(event, "Content-Type", "image/x-icon");
-      return send(
-        event,
-        "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
-      );
-    }
+    const nitroApp = useNitroApp();
 
-    const response = await handler(event);
-    if (!response) {
+    // Create shared context for hooks
+    const ctx: RenderContext = { event, render, response: undefined };
+
+    // Call initial hook to prepare and optionally custom render
+    await nitroApp.hooks.callHook("render:before", ctx);
+
+    if (!ctx.response /* not handled by hook */) {
+      // TODO: Use serve-placeholder
+      if (event.path === `${runtimeConfig.app.baseURL}favicon.ico`) {
+        setResponseHeader(event, "Content-Type", "image/x-icon");
+        return send(
+          event,
+          "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
+        );
+      }
+
+      ctx.response = await ctx.render(event);
+
       const _currentStatus = getResponseStatus(event);
       setResponseStatus(event, _currentStatus === 200 ? 500 : _currentStatus);
       return send(
@@ -33,23 +42,22 @@ export function defineRenderHandler(handler: RenderHandler) {
       );
     }
 
-    // Allow hooking and modifying response
-    const nitroApp = useNitroApp();
-    await nitroApp.hooks.callHook("render:response", response, { event });
-
-    // TODO: Warn if response is already handled
-
-    // TODO: Caching support
+    // Allow  modifying response
+    await nitroApp.hooks.callHook("render:response", ctx.response, ctx);
 
     // Send headers
-    if (response.headers) {
-      setResponseHeaders(event, response.headers);
+    if (ctx.response.headers) {
+      setResponseHeaders(event, ctx.response.headers);
     }
-    if (response.statusCode || response.statusMessage) {
-      setResponseStatus(event, response.statusCode, response.statusMessage);
+    if (ctx.response.statusCode || ctx.response.statusMessage) {
+      setResponseStatus(
+        event,
+        ctx.response.statusCode,
+        ctx.response.statusMessage
+      );
     }
 
     // Send response body
-    return response.body;
+    return ctx.response.body;
   });
 }

src/core/index.ts
@@ -5,6 +5,7 @@ import type {
   NitroOpenAPIConfig,
   NitroRouteConfig,
   RenderResponse,
+  RenderContext,
 } from "nitropack/types";
 
 // Core
@@ -79,9 +80,11 @@ export interface NitroRuntimeHooks {
   beforeResponse: NonNullable<AppOptions["onBeforeResponse"]>;
   afterResponse: NonNullable<AppOptions["onAfterResponse"]>;
 
+  "render:before": (context: RenderContext) => void;
+
   "render:response": (
     response: Partial<RenderResponse>,
-    context: { event: H3Event }
+    context: RenderContext
   ) => void;
 }
 

src/types/runtime/nitro.ts
@@ -34,6 +34,12 @@ export type RenderHandler = (
   event: H3Event
 ) => Partial<RenderResponse> | Promise<Partial<RenderResponse>>;
 
+export interface RenderContext {
+  event: H3Event;
+  render: RenderHandler;
+  response?: Partial<RenderResponse>;
+}
+
 export interface CapturedErrorContext {
   event?: H3Event;
   [key: string]: unknown;

commit 7a97b0a233dda0777f68997b10869ebdd7ebfe62

fix(renderer): check `ctx.response`
regression from #2770

src/runtime/internal/renderer.ts
@@ -34,12 +34,14 @@ export function defineRenderHandler(render: RenderHandler) {
 
       ctx.response = await ctx.render(event);
 
-      const _currentStatus = getResponseStatus(event);
-      setResponseStatus(event, _currentStatus === 200 ? 500 : _currentStatus);
-      return send(
-        event,
-        "No response returned from render handler: " + event.path
-      );
+      if (!ctx.response) {
+        const _currentStatus = getResponseStatus(event);
+        setResponseStatus(event, _currentStatus === 200 ? 500 : _currentStatus);
+        return send(
+          event,
+          "No response returned from render handler: " + event.path
+        );
+      }
     }
 
     // Allow  modifying response