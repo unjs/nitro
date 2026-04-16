7e30f59957632078856ff58258f9de3f89394aff
Sandro Circi <sandro.circi@digitoolmedia.com>
2026-04-16 11:08:39 +0200

    chore: update bunny-preset to use upstream implementations

--- src/presets/bunny/runtime/edge-scripting.ts
+++ src/presets/bunny/runtime/edge-scripting.ts
@@ -1,20 +1,42 @@
 import "#nitro/virtual/polyfills";
+import type { ServerRequest } from "srvx";
+import { serve } from "srvx/bunny";
+import wsAdapter from "crossws/adapters/bunny";
+
 import { useNitroApp } from "nitro/app";
+import { resolveWebsocketHooks } from "#nitro/runtime/app";
+import { tracingSrvxPlugins } from "#nitro/virtual/tracing";
+import { startScheduleRunner } from "#nitro/runtime/task";
+
+const _parsedPort = Number.parseInt(process.env.NITRO_PORT ?? process.env.PORT ?? "");
+
+const port = Number.isNaN(_parsedPort) ? 3000 : _parsedPort;
+const host = process.env.NITRO_HOST || process.env.HOST;
 
 const nitroApp = useNitroApp();
 
-// @ts-expect-error
-if (typeof Bunny !== "undefined") {
-  // @ts-expect-error
-  Bunny.v1.serve(nitroApp.fetch);
-} else {
-  const _parsedPort = Number.parseInt(process.env.NITRO_PORT ?? process.env.PORT ?? "");
+let _fetch = nitroApp.fetch;
 
-  Deno.serve(
-    {
-      port: Number.isNaN(_parsedPort) ? 3000 : _parsedPort,
-      hostname: process.env.NITRO_HOST || process.env.HOST,
-    },
-    nitroApp.fetch
-  );
+if (import.meta._websocket) {
+  const { handleUpgrade } = wsAdapter({ resolve: resolveWebsocketHooks });
+  _fetch = (req: ServerRequest) => {
+    if (req.headers.get("upgrade") === "websocket") {
+      return handleUpgrade(req);
+    }
+    return nitroApp.fetch(req);
+  };
 }
+
+const server = serve({
+  port,
+  hostname: host,
+  fetch: _fetch,
+  plugins: [...tracingSrvxPlugins],
+});
+
+// Scheduled tasks
+if (import.meta._tasks) {
+  startScheduleRunner({ waitUntil: server.waitUntil });
+}
+
+export default {};
